bb46354f8aea219bf35e244988444322628607ff
Alex Pascal <apascal07@gmail.com>
2024-01-17 11:04:05 -0800

    Added basic Runner API. (#1)
    * Added basic Runner API.
    
    * Fixed package-lock.json.

---
+++ genkit-tools/src/runner/runner.ts
@@ -0,0 +1,58 @@
+import * as express from "express";
+import { ErrorRequestHandler } from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+const t = initTRPC.create();
+
+export const runnerRouter = t.router({
+  // Returns all runnable flows.
+  flows: t.procedure
+    .query(() => {
+      throw new TRPCError({
+        code: "NOT_IMPLEMENTED",
+        message: "This endpoint is not implemented yet.",
+      });
+    }),
+  // Runs a flow.
+  runFlow: t.procedure
+    .input(z.object({ id: z.string() }))
+    .mutation((input) => {
+      throw new TRPCError({
+        code: "NOT_IMPLEMENTED",
+        message: "This endpoint is not implemented yet.",
+      });
+    }),
+  // Reloads all code.
+  reload: t.procedure
+    .input(z.object({ name: z.string().optional() }).optional())
+    .mutation(() => {
+      throw new TRPCError({
+        code: "NOT_IMPLEMENTED",
+        message: "This endpoint is not implemented yet.",
+      });
+    }),
+});
+
+export type RunnerRouter = typeof runnerRouter;
+
+async function startRunner() {
+  var app = express();
+  app.use(express.json());
+  const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(err);
+  };
+  app.use(errorHandler);
+  const PORT = process.env["PORT"] || 3000;
+  app.use(
+    "/api",
+    trpcExpress.createExpressMiddleware({
+      router: runnerRouter,
+    })
+  );
+  app.listen(PORT, () => {
+    console.log(`Runner API running on http://localhost:${PORT}/api`);
+  });
+}
