ff89d48481d4f1b217f071256a37b327c6d4ab6b
Michael Bleigh <bleigh@google.com>
2024-02-01 20:31:07 -0800

    Adds generic model implementation.

---
+++ genkit/ai/src/generate.ts
@@ -0,0 +1,167 @@
+import zodToJsonSchema from 'zod-to-json-schema';
+import {
+  CandidateData,
+  GenerationConfig,
+  GenerationRequest,
+  GenerationResponseData,
+  GenerationUsage,
+  MessageData,
+  ModelAction,
+  Part,
+  ToolDefinition,
+} from './model';
+import { extractJson } from './extract';
+import { Action } from '@google-genkit/common';
+import { z } from 'zod';
+import { lookup } from '@google-genkit/common/registry';
+
+export class Message<T = unknown> implements MessageData {
+  role: MessageData['role'];
+  content: Part[];
+
+  constructor(message: MessageData) {
+    this.role = message.role;
+    this.content = message.content;
+  }
+
+  output(): T | null {
+    return extractJson<T>(this.text());
+  }
+
+  text(): string {
+    return this.content.map((part) => part.text || '').join('');
+  }
+}
+
+export class Candidate<O = unknown> implements CandidateData {
+  message: Message<O>;
+  index: number;
+  usage: GenerationUsage;
+  finishReason: CandidateData['finishReason'];
+  finishMessage: string;
+  custom: unknown;
+
+  output(): O | null {
+    return this.message.output();
+  }
+
+  text(): string {
+    return this.message.text();
+  }
+
+  constructor(candidate: CandidateData) {
+    this.message = new Message(candidate.message);
+    this.index = candidate.index;
+    this.usage = candidate.usage || {};
+    this.finishReason = candidate.finishReason;
+    this.finishMessage = candidate.finishMessage || '';
+    this.custom = candidate.custom;
+  }
+}
+
+export class GenerationResponse<O = unknown> implements GenerationResponseData {
+  candidates: Candidate<O>[];
+  usage: GenerationUsage;
+  custom: unknown;
+
+  output(): O | null {
+    return this.candidates[0]?.output() || null;
+  }
+
+  text(): string {
+    return this.candidates[0]?.text() || '';
+  }
+
+  constructor(response: GenerationResponseData) {
+    this.candidates = (response.candidates || []).map(
+      (candidate) => new Candidate(candidate)
+    );
+    this.usage = response.usage || {};
+    this.custom = response.custom || {};
+  }
+}
+
+function toToolDefinition(
+  tool: Action<z.ZodTypeAny, z.ZodTypeAny>
+): ToolDefinition {
+  return {
+    name: tool.__action.name,
+    outputSchema: zodToJsonSchema(tool.__action.outputSchema!),
+    inputSchema: zodToJsonSchema(tool.__action.inputSchema!),
+  };
+}
+
+function toGenerateRequest(prompt: ModelPrompt): GenerationRequest {
+  const promptMessage: MessageData = { role: 'user', content: [] };
+  if (typeof prompt.prompt === 'string') {
+    promptMessage.content.push({ text: prompt.prompt });
+  } else if (Array.isArray(prompt.prompt)) {
+    promptMessage.content.push(...prompt.prompt);
+  } else {
+    promptMessage.content.push(prompt.prompt);
+  }
+
+  if (prompt.output?.schema) {
+    const outputSchema = zodToJsonSchema(prompt.output.schema);
+    promptMessage.content.push({
+      text: `
+    
+Output should be JSON formatted and conform to the following schema:
+
+\`\`\`
+${JSON.stringify(outputSchema)}
+\`\`\``,
+    });
+  }
+
+  const messages: MessageData[] = [...(prompt.history || []), promptMessage];
+
+  return {
+    messages,
+    candidates: prompt.candidates,
+    config: prompt.config,
+    tools: prompt.tools?.map((tool) => toToolDefinition(tool)) || [],
+    output: {
+      format:
+        prompt.output?.format || (prompt.output?.schema ? 'json' : 'text'),
+      schema: prompt.output?.schema
+        ? zodToJsonSchema(prompt.output.schema)
+        : undefined,
+    },
+  };
+}
+
+export interface ModelPrompt<
+  O extends z.ZodTypeAny = z.ZodTypeAny,
+  CustomOptions extends z.ZodTypeAny = z.ZodTypeAny
+> {
+  model: ModelAction<CustomOptions> | string;
+  prompt: string | Part | Part[];
+  history?: MessageData[];
+  tools?: Action<z.ZodTypeAny, z.ZodTypeAny>[];
+  candidates?: number;
+  config: GenerationConfig<z.infer<CustomOptions>>;
+  output?: { format?: 'text' | 'json'; schema?: O };
+}
+
+export async function generate<
+  O extends z.ZodTypeAny = z.ZodTypeAny,
+  CustomOptions extends z.ZodTypeAny = z.ZodTypeAny
+>(
+  prompt: ModelPrompt<O, CustomOptions>
+): Promise<GenerationResponse<z.infer<O>>> {
+  let model: ModelAction<CustomOptions>;
+  if (typeof prompt.model === 'string') {
+    model = lookup(`models/${prompt.model}`);
+  } else {
+    model = prompt.model;
+  }
+
+  const request = toGenerateRequest(prompt);
+  const response = new GenerationResponse<z.infer<O>>(await model(request));
+  if (prompt.output?.schema) {
+    const outputData = response.output();
+    prompt.output.schema.parse(outputData);
+  }
+  return response;
+}
---
+++ genkit/ai/src/model.ts
@@ -0,0 +1,140 @@
+import { Action, action } from '@google-genkit/common';
+import { registerAction } from '@google-genkit/common/registry';
+import { z } from 'zod';
+
+export const PartSchema = z.object({
+  text: z.string().optional(),
+  media: z
+    .object({
+      contentType: z.string().optional(),
+      uri: z
+        .string()
+        .describe('data: or https: uri containing the media content'),
+    })
+    .optional(),
+  tool: z
+    .object({
+      name: z.string(),
+      input: z.record(z.unknown()).optional(),
+      output: z.record(z.unknown()).optional(),
+    })
+    .optional(),
+});
+export type Part = z.infer<typeof PartSchema>;
+
+export const RoleSchema = z.enum(['system', 'user', 'model', 'tool']);
+export type Role = z.infer<typeof RoleSchema>;
+
+export const MessageSchema = z.object({
+  role: RoleSchema,
+  content: z.array(PartSchema),
+});
+export type MessageData = z.infer<typeof MessageSchema>;
+
+export const ModelInfoSchema = z.object({
+  names: z.array(z.string()),
+  label: z.string().optional(),
+  match: z.function(z.tuple([z.string()]), z.boolean()).optional(),
+});
+export type ModelInfo = z.infer<typeof ModelInfoSchema>;
+
+export const ToolDefinitionSchema = z.object({
+  name: z.string(),
+  inputSchema: z
+    .record(z.any())
+    .describe('Valid JSON Schema representing the input of the tool.'),
+  outputSchema: z
+    .record(z.any())
+    .describe('Valid JSON Schema describing the output of the tool.'),
+});
+export type ToolDefinition = z.infer<typeof ToolDefinitionSchema>;
+
+export const GenerationConfig = z.object({
+  temperature: z.number().optional(),
+  maxOutputTokens: z.number().optional(),
+  topK: z.number().optional(),
+  topP: z.number().optional(),
+  custom: z.record(z.any()).optional(),
+  stopSequences: z.array(z.string()).optional(),
+});
+export type GenerationConfig<CustomOptions = any> = z.infer<
+  typeof GenerationConfig
+> & {
+  custom?: CustomOptions;
+};
+
+const OutputConfigSchema = z.object({
+  format: z.enum(['json', 'text']).optional(),
+  schema: z.record(z.any()).optional(),
+});
+export type OutputConfig = z.infer<typeof OutputConfigSchema>;
+
+export const GenerationRequestSchema = z.object({
+  messages: z.array(MessageSchema),
+  config: GenerationConfig.optional(),
+  tools: z.array(ToolDefinitionSchema).optional(),
+  output: OutputConfigSchema.optional(),
+  candidates: z.number().optional(),
+});
+export type GenerationRequest = z.infer<typeof GenerationRequestSchema>;
+
+export const GenerationUsageSchema = z.object({
+  inputTokens: z.number().optional(),
+  outputTokens: z.number().optional(),
+  totalTokens: z.number().optional(),
+  custom: z.record(z.number()).optional(),
+});
+export type GenerationUsage = z.infer<typeof GenerationUsageSchema>;
+
+export const CandidateSchema = z.object({
+  index: z.number(),
+  message: MessageSchema,
+  usage: GenerationUsageSchema.optional(),
+  finishReason: z.enum(['stop', 'length', 'blocked', 'other', 'unknown']),
+  finishMessage: z.string().optional(),
+  custom: z.unknown(),
+});
+export type CandidateData = z.infer<typeof CandidateSchema>;
+
+export const CandidateErrorSchema = z.object({
+  index: z.number(),
+  code: z.enum(['blocked', 'other', 'unknown']),
+  message: z.string().optional(),
+});
+export type CandidateError = z.infer<typeof CandidateErrorSchema>;
+
+export const GenerationResponseSchema = z.object({
+  candidates: z.array(CandidateSchema),
+  usage: GenerationUsageSchema.optional(),
+  custom: z.unknown(),
+});
+export type GenerationResponseData = z.infer<typeof GenerationResponseSchema>;
+
+export type ModelAction<
+  CustomOptionsSchema extends z.ZodTypeAny = z.ZodTypeAny
+> = Action<typeof GenerationRequestSchema, typeof GenerationResponseSchema> & {
+  __customOptionsType: CustomOptionsSchema;
+};
+
+export function modelAction<
+  CustomOptionsSchema extends z.ZodTypeAny = z.ZodTypeAny
+>(
+  options: {
+    name: string;
+    customOptionsType?: CustomOptionsSchema;
+  },
+  runner: (request: GenerationRequest) => Promise<GenerationResponseData>
+): ModelAction<CustomOptionsSchema> {
+  const act = action(
+    {
+      name,
+      description: `${name} GenAI model`,
+      input: GenerationRequestSchema,
+      output: GenerationResponseSchema,
+    },
+    runner
+  );
+  (act as any).__customOptionsType = options.customOptionsType || z.any();
+  registerAction('model', name, act);
+  return act as ModelAction<CustomOptionsSchema>;
+}
