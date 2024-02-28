af5ac73ef19a462ddc4edf7687c9cf4a4fd7968b
Pavel Jbanov <pavelj@google.com>
2024-02-28 16:28:22 -0500

    basic extract eval data command (#185)

---
+++ genkit-tools/src/commands/eval-extract-data.ts
@@ -0,0 +1,42 @@
+import { Command } from 'commander';
+import { startRunner } from '../utils/runner-utils';
+import { logger } from '../utils/logger';
+
+interface EvalDatasetOptions {
+  env: 'dev' | 'prod';
+}
+
+/** Command to extract evaluation data. */
+export const evalExtractData = new Command('eval:extractData')
+  .argument('<flowName>', 'name of the flow to run')
+  .option('--env <env>', 'environment (dev/prod)', 'dev')
+  .action(async (flowName: string, options: EvalDatasetOptions) => {
+    const runner = await startRunner();
+
+    logger.info(`Extracting trace data '/flow/${flowName}'...`);
+    const traces = await runner.listTraces({
+      env: options.env,
+    });
+
+    var dataset = traces.map((t) => {
+      const rootSpan = Object.values(t.spans).find(
+        (s) =>
+          s.attributes['genkit:type'] === 'flow' &&
+          t.displayName === flowName &&
+          s.attributes['genkit:metadata:flow:state'] === 'done'
+      );
+      if (!rootSpan) {
+        return undefined;
+      }
+      // TODO: add context extraction
+      return {
+        input: rootSpan?.attributes['genkit:input'],
+        output: rootSpan?.attributes['genkit:output'],
+      };
+    });
+    dataset = dataset.filter((d) => !!d);
+
+    logger.info(JSON.stringify(dataset, undefined, '  '));
+
+    await runner.stop();
+  });
