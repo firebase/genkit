07a25a6b82838bdcaabe4d2c385c1f23d9095ec4
Sam Phillips <samphillips@google.com>
2024-03-04 23:02:13 -0500

    Fix the coffeeshop's vertexAI example, move main() code into a flow (#226)

--- genkit/samples/coffee-shop/src/genkit.conf.ts
+++ genkit/samples/coffee-shop/src/genkit.conf.ts
@@ -1,11 +1,15 @@
 import { getProjectId } from '@google-genkit/common';
 import { configureGenkit } from '@google-genkit/common/config';
 import { openAI } from '@google-genkit/plugin-openai';
-import { googleAI } from '@google-genkit/providers/google-ai';
+import { vertexAI } from '@google-genkit/plugin-vertex-ai';
 import { firebase } from '@google-genkit/providers/firebase';
 
 export default configureGenkit({
-  plugins: [firebase({ projectId: getProjectId() }), googleAI(), openAI()],
+  plugins: [
+    firebase({ projectId: getProjectId() }),
+    vertexAI({ projectId: getProjectId(), location: 'us-central1' }),
+    openAI(),
+  ],
   flowStateStore: 'firebase',
   traceStore: 'firebase',
   enableTracingAndMetrics: true,
