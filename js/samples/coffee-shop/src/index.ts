09cfd536e79a3b738b2d82cf0fc6879c83f10eeb
Michael Doyle <michaeldoyle@google.com>
2024-03-18 11:55:52 -0400

    Make Recommender With History camelCase instead (#418)

--- js/samples/coffee-shop/src/index.ts
+++ js/samples/coffee-shop/src/index.ts
@@ -167,7 +167,7 @@ export const basicCoffeeRecommender = flow(
 
 export const historyCoffeeRecommender = flow(
   {
-    name: 'Recommender with history',
+    name: 'recommenderWithHistory',
     input: SimpleOrderHistorySchema,
     output: z.string(),
   },
