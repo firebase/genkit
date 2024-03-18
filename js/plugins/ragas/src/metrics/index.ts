e1d765819cccfa3a236d5f3df6f7ea692fb55667
ssbushi <66321939+ssbushi@users.noreply.github.com>
2024-03-18 21:20:42 +0000

    Refactor ragas to third_party (#433)
    * refactor ragas to third_party
    
    * update metadata

--- js/plugins/ragas/src/metrics/index.ts
+++
@@ -1,3 +0,0 @@
-export { faithfulnessScore } from './faithfulness';
-export { answerRelevancyScore } from './answer_relevancy';
-export { contextPrecisionScore } from './context_precision';
--- js/plugins/ragas/tsup.config.ts
+++
@@ -1,6 +0,0 @@
-import { Options, defineConfig } from 'tsup';
-import { defaultOptions } from '../../tsup.common';
-
-export default defineConfig({
-  ...(defaultOptions as Options),
-});
