f415abd6f5c71a419c6e61d4c72670c84df8e0a1
MaesterChestnut <40321652+MaesterChestnut@users.noreply.github.com>
2024-03-25 16:28:29 -0400

    Remove FlowState error formatting until can tackle holistically (#567)

--- genkit-tools/src/commands/flow-run.ts
+++ genkit-tools/src/commands/flow-run.ts
@@ -72,16 +72,7 @@ export const flowRun = new Command('flow:run')
       logger.info(
         'Flow operation:\n' + JSON.stringify(state.operation, undefined, '  ')
       );
-      if (state.operation.result?.error) {
-        logger.error(
-          'Error:\n' +
-            JSON.stringify(state.operation.result?.error, undefined, '  ')
-        );
-        logger.error(
-          'Stack Trace:\n' +
-            JSON.stringify(state.operation.result?.stacktrace, undefined, '  ')
-        );
-      }
+
       if (options.output && state.operation.result?.response) {
         await writeFile(
           options.output,
