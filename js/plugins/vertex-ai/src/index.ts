a88f9699ac642ae8d045e613fe3413db5b9288fa
huangjeff5 <64040981+huangjeff5@users.noreply.github.com>
2024-04-02 14:01:09 -0400

    Add additional embedder models to vertex plugin (#739)

--- js/plugins/vertex-ai/src/index.ts
+++ js/plugins/vertex-ai/src/index.ts
@@ -18,7 +18,11 @@ import { getLocation, getProjectId } from '@genkit-ai/core';
 import { Plugin, genkitPlugin } from '@genkit-ai/core/config';
 import { VertexAI } from '@google-cloud/vertexai';
 import { GoogleAuth } from 'google-auth-library';
-import { textEmbeddingGecko, textEmbeddingGeckoEmbedder } from './embedder.js';
+import {
+  SUPPORTED_EMBEDDER_MODELS,
+  textEmbeddingGecko,
+  textEmbeddingGeckoEmbedder,
+} from './embedder.js';
 import {
   SUPPORTED_GEMINI_MODELS,
   geminiModel,
@@ -62,7 +66,9 @@ export const vertexAI: Plugin<[PluginOptions] | []> = genkitPlugin(
         ),
       ],
       embedders: [
-        textEmbeddingGeckoEmbedder(authClient, { projectId, location }),
+        ...Object.keys(SUPPORTED_EMBEDDER_MODELS).map((name) =>
+          textEmbeddingGeckoEmbedder(name, authClient, { projectId, location })
+        ),
       ],
     };
   }
