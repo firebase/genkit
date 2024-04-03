4695c0ca359590202937344b9a0a807ed61009cc
Michael Doyle <michaeldoyle@google.com>
2024-04-03 15:39:45 -0400

    Permissive safety settings for the cat evals sample (#801)
    In general, this is how you would avoid evals getting blocked

--- js/samples/cat-eval/src/pdf_rag.ts
+++ js/samples/cat-eval/src/pdf_rag.ts
@@ -67,6 +67,28 @@ export const pdfQA = defineFlow(
     const llmResponse = await generate({
       model: geminiPro,
       prompt: { text: augmentedPrompt },
+      config: {
+        custom: {
+          safetySettings: [
+            {
+              category: 'HARM_CATEGORY_HATE_SPEECH',
+              threshold: 'BLOCK_NONE',
+            },
+            {
+              category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+              threshold: 'BLOCK_NONE',
+            },
+            {
+              category: 'HARM_CATEGORY_HARASSMENT',
+              threshold: 'BLOCK_NONE',
+            },
+            {
+              category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+              threshold: 'BLOCK_NONE',
+            },
+          ],
+        },
+      },
     });
     return llmResponse.text();
   }
