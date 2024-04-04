9c99cc78a079b68043ac528b813a3614fcb1124e
Pavel Jbanov <pavelj@google.com>
2024-04-04 16:07:20 -0400

    Removed discovery.js and hardcoded tools server discovery to dev mode (#833)

--- genkit-tools/src/server/index.ts
+++ genkit-tools/src/server/index.ts
@@ -18,7 +18,6 @@ import * as trpcExpress from '@trpc/server/adapters/express';
 import * as bodyParser from 'body-parser';
 import * as clc from 'colorette';
 import express, { ErrorRequestHandler } from 'express';
-import { writeFileSync } from 'fs';
 import open from 'open';
 import path from 'path';
 import { Runner } from '../runner/runner';
@@ -31,22 +30,7 @@ const UI_STATIC_FILES_DIR = path.resolve(
   __dirname,
   '../client/dist/ui/browser'
 );
-const UI_DEVELOPMENT_FILES_DIR = path.resolve(__dirname, '../../ui/src/assets');
 const API_BASE_PATH = '/api';
-
-/**
- * Writes a small JS file containing the CLI port number, optionally to the
- * development environment so that `ng serve` works as expected as well.
- */
-function generateDiscoverabilityFile(headless: boolean, port: number): void {
-  const basePath = headless
-    ? UI_DEVELOPMENT_FILES_DIR
-    : path.resolve(UI_STATIC_FILES_DIR, 'assets');
-  writeFileSync(
-    path.join(basePath, 'discovery.js'),
-    `(() => window._tools_server_url_ = ${headless ? `"http://localhost:${port}"` : '""'})();`
-  );
-}
 
 /**
  * Starts up the Genkit Tools server which includes static files for the UI and the Tools API.
@@ -60,8 +44,6 @@ export function startServer(
   const enderPromise = new Promise<void>((resolver) => {
     serverEnder = resolver;
   });
-
-  generateDiscoverabilityFile(headless, port);
 
   const app = express();
 
