7c1da95877290868322522cebf4d0b87c7a80a76
Ian Lance Taylor <iant@golang.org>
2024-04-10 13:36:14 -0700

    [ Go ] define Embedder and Retriever interfaces (#35)
    The intent is that the actual implementations of embedders and
    retrievers will implement these interfaces. They will be wrapped in
    actions for invocation from the genkit tool.
    
    A retriever will use the embedder interface to embed a Document.
    We may want to have a way for the retriever to invoke an embedder action,
    so that tracing works nicely.

---
+++ go/genkit/retriever.go
@@ -0,0 +1,60 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package genkit
+
+import (
+	"context"
+)
+
+// Retriever supports adding documents to a database, and
+// retrieving documents from the database that are similar to a query document.
+// Vector databases will implement this interface.
+type Retriever interface {
+	// Add a document to the database.
+	Index(context.Context, *IndexerRequest) error
+	// Retrieve matching documents from the database.
+	Retrieve(context.Context, *RetrieverRequest) (*RetrieverResponse, error)
+}
+
+// IndexerRequest is the data we pass to add documents to the database.
+// The Options field is specific to the actual retriever implementation.
+type IndexerRequest struct {
+	Documents []*Document `json:"docs"`
+	Options   any         `json:"options,omitempty"`
+}
+
+// RetrieverRequest is the data we pass to retrieve documents from the database.
+// The Options field is specific to the actual retriever implementation.
+type RetrieverRequest struct {
+	Document *Document `json:"content"`
+	Options  any       `json:"options,omitempty"`
+}
+
+// RetrieverResponse is the response to a document lookup.
+type RetrieverResponse struct {
+	Documents []*Document `json:"documents"`
+}
+
+// RegisterRetriever registers the actions for a specific retriever.
+func RegisterRetriever(name string, retriever Retriever) {
+	RegisterAction(ActionTypeRetriever, name,
+		NewAction(name, retriever.Retrieve))
+
+	RegisterAction(ActionTypeIndexer, name,
+		NewAction(name, func(ctx context.Context, req *IndexerRequest) (struct{}, error) {
+			err := retriever.Index(ctx, req)
+			return struct{}{}, err
+		}))
+}
