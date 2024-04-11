bab00b640a9e6fc307d65153c22bc0884d695328
Ian Lance Taylor <iant@golang.org>
2024-04-11 13:43:52 -0700

    [Go] add fake embedder implementation for testing (#48)
    * [Go] add fake embedder implementation for testing
    
    * move files up one level

---
+++ go/internal/fakeembedder/fakeembedder_test.go
@@ -0,0 +1,51 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fakeembedder
+
+import (
+	"context"
+	"slices"
+	"testing"
+
+	"github.com/google/genkit/go/genkit"
+)
+
+func TestFakeEmbedder(t *testing.T) {
+	embed := New()
+
+	d := genkit.DocumentFromText("fakeembedder test", nil)
+
+	vals := []float32{1, 2}
+	embed.Register(d, vals)
+
+	var genkitEmbedder genkit.Embedder
+	genkitEmbedder = embed
+	req := &genkit.EmbedRequest{
+		Document: d,
+	}
+	ctx := context.Background()
+	got, err := genkitEmbedder.Embed(ctx, req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !slices.Equal(got, vals) {
+		t.Errorf("lookup returned %v, want %v", got, vals)
+	}
+
+	req.Document = genkit.DocumentFromText("missing document", nil)
+	if _, err = genkitEmbedder.Embed(ctx, req); err == nil {
+		t.Error("embedding unknown document succeeded unexpectedly")
+	}
+}
