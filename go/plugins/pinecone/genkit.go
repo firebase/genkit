8002374057a79db57b1a3beb7ff34e528456dce0
Ian Lance Taylor <iant@golang.org>
2024-04-15 09:45:06 -0700

    [Go] add genkit support to pinecone client (#52)
    Now calling pinecone.Init will register genkit actions.

---
+++ go/plugins/pinecone/genkit.go
@@ -0,0 +1,264 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pinecone
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/google/genkit/go/genkit"
+)
+
+// defaultTextKey is the default metadata key we use to store the
+// document text as metadata of the documented stored in pinecone.
+// This lets us map back from the document returned by a query to
+// the text. In other words, rather than remembering the mapping
+// between documents and pinecone data ourselves, we just store the
+// documents in pinecone.
+const defaultTextKey = "_content"
+
+// Init registers actions to be used with genkit.
+//
+// apiKey is the API key to use to access Pinecone.
+// If it is the empty string, it is read from the PINECONE_API_INDEX
+// environment variable.
+//
+// host is the controller host name. This implies which index to use.
+// If it is the empty string, it is read from the PINECONE_CONTROLLER_HOST
+// environment variable.
+//
+// The caller must pass an Embedder to use.
+//
+// The textKey parameter is the metadata key to use to store document text
+// in Pinecone; the default is "_content".
+func Init(ctx context.Context, apiKey, host string, embedder genkit.Embedder, embedderOptions any, textKey string) error {
+	r, err := newRetriever(ctx, apiKey, host, embedder, embedderOptions, textKey)
+	if err != nil {
+		return err
+	}
+	genkit.RegisterRetriever("pinecone", r)
+	return nil
+}
+
+// newRetriever returns a new genkit.Retriever to register.
+func newRetriever(ctx context.Context, apiKey, host string, embedder genkit.Embedder, embedderOptions any, textKey string) (genkit.Retriever, error) {
+	client, err := NewClient(ctx, apiKey)
+	if err != nil {
+		return nil, err
+	}
+	index, err := client.Index(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+	if textKey == "" {
+		textKey = defaultTextKey
+	}
+	r := &retriever{
+		index:           index,
+		embedder:        embedder,
+		embedderOptions: embedderOptions,
+		textKey:         textKey,
+	}
+	return r, nil
+}
+
+// IndexerOptions may be passed in the Options field
+// of [genkit.IndexerRequest] to pass options to Pinecone.
+// The Options field should be either nil or a value of type *IndexerOptions.
+type IndexerOptions struct {
+	Namespace string `json:"namespace,omitempty"` // Pinecone namespace to use
+}
+
+// RetrieverOptions may be passed in the Options field
+// of [genkit.RetrieverRequest] to pass options to Pinecone.
+// The Options field should be either nil or a value of type *RetrieverOptions.
+type RetrieverOptions struct {
+	Namespace string `json:"namespace,omitempty"` // Pinecone namespace to use
+	Count     int `json:"count,omitempty"`        // maximum number of values to retrieve
+}
+
+// retriever implements the genkit Retriever interface.
+type retriever struct {
+	index           *Index
+	embedder        genkit.Embedder
+	embedderOptions any
+	textKey         string
+}
+
+// Index implements the genkit Retriever.Index method.
+func (r *retriever) Index(ctx context.Context, req *genkit.IndexerRequest) error {
+	if len(req.Documents) == 0 {
+		return nil
+	}
+
+	namespace := ""
+	if req.Options != nil {
+		// TODO(iant): This is plausible when called directly
+		// from Go code, but what will it look like when
+		// run from a resumed flow?
+		options, ok := req.Options.(*IndexerOptions)
+		if !ok {
+			return fmt.Errorf("pinecone.Index options have type %T, want %T", req.Options, &IndexerOptions{})
+		}
+		namespace = options.Namespace
+	}
+
+	// Use the embedder to convert each Document into a vector.
+	vecs := make([]Vector, 0, len(req.Documents))
+	for _, doc := range req.Documents {
+		ereq := &genkit.EmbedRequest{
+			Document: doc,
+			Options:  r.embedderOptions,
+		}
+		vals, err := r.embedder.Embed(ctx, ereq)
+		if err != nil {
+			return fmt.Errorf("pinecone index embedding failed: %v", err)
+		}
+
+		id, err := docID(doc)
+		if err != nil {
+			return err
+		}
+
+		var metadata map[string]any
+		if doc.Metadata == nil {
+			metadata = make(map[string]any)
+		} else {
+			metadata = maps.Clone(doc.Metadata)
+		}
+		// TODO(iant): This seems to be what the TypeScript code does,
+		// but it loses the structure of the document.
+		var sb strings.Builder
+		for _, p := range doc.Content {
+			sb.WriteString(p.Text())
+		}
+		metadata[r.textKey] = sb.String()
+
+		v := Vector{
+			ID: id,
+			Values: vals,
+			Metadata: metadata,
+		}
+		vecs = append(vecs, v)
+	}
+
+	if err := r.index.Upsert(ctx, vecs, namespace); err != nil {
+		return err
+	}
+
+	// Pinecone is only eventually consistent.
+	// Wait until the vectors are visible.
+	wait := func() (bool, error) {
+		delay := 10 * time.Millisecond
+		for i := 0; i < 20; i++ {
+			vec, err := r.index.QueryByID(ctx, vecs[0].ID, WantValues, namespace)
+			if err != nil {
+				return false, err
+			}
+			if vec != nil {
+				// For some reason Pinecone doesn't
+				// reliably return a vector with the
+				// same ID.
+				for _, v := range vecs {
+					if vec.ID == v.ID && slices.Equal(vec.Values, v.Values) {
+						return true, nil
+					}
+				}
+			}
+			time.Sleep(delay)
+			delay *= 2
+		}
+		return false, nil
+	}
+	seen, err := wait()
+	if err != nil {
+		return err
+	}
+	if !seen {
+		return errors.New("inserted Pinecone records never became visible")
+	}
+
+	return nil
+}
+
+// Retrieve implements the genkit Retriever.Retrieve method.
+func (r *retriever) Retrieve(ctx context.Context, req *genkit.RetrieverRequest) (*genkit.RetrieverResponse, error) {
+	var (
+		namespace string
+		count     int
+	)
+	if req.Options != nil {
+		// TODO(iant): This is plausible when called directly
+		// from Go code, but what will it look like when
+		// run from a resumed flow?
+		ropt, ok := req.Options.(*RetrieverOptions)
+		if !ok {
+			return nil, fmt.Errorf("pinecone.Retrieve options have type %T, want %T", req.Options, &RetrieverOptions{})
+		}
+		namespace = ropt.Namespace
+		count = ropt.Count
+	}
+
+	// Use the embedder to convert the document we want to
+	// retrieve into a vector.
+	ereq := &genkit.EmbedRequest{
+		Document: req.Document,
+		Options:  r.embedderOptions,
+	}
+	vals, err := r.embedder.Embed(ctx, ereq)
+	if err != nil {
+		return nil, fmt.Errorf("pinecone retrieve embedding failed: %v", err)
+	}
+
+	results, err := r.index.Query(ctx, vals, count, WantMetadata, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	var docs []*genkit.Document
+	for _, result := range results {
+		text, _ := result.Metadata[r.textKey].(string)
+		if text == "" {
+			return nil, errors.New("Pinecone retrieve failed to fetch original document text")
+		}
+		delete(result.Metadata, r.textKey)
+		// TODO(iant): This is what the TypeScript code does,
+		// but it loses information for multimedia documents.
+		d := genkit.DocumentFromText(text, result.Metadata)
+		docs = append(docs, d)
+	}
+
+	ret := &genkit.RetrieverResponse{
+		Documents: docs,
+	}
+	return ret, nil
+}
+
+// docID returns the ID to use for a Document.
+// This is intended to be the same as the genkit Typescript computation.
+func docID(doc *genkit.Document) (string, error) {
+	b, err := json.Marshal(doc)
+	if err != nil {
+		return "", fmt.Errorf("pinecone: error marshaling document: %v", err)
+	}
+	return fmt.Sprintf("%02x", md5.Sum(b)), nil
+}
---
+++ go/plugins/pinecone/genkit_test.go
@@ -0,0 +1,140 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pinecone
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/google/genkit/go/genkit"
+	"github.com/google/genkit/go/internal/fakeembedder"
+)
+
+func TestGenkit(t *testing.T) {
+	if *testAPIKey == "" {
+		t.Skip("skipping test because -test-pinecone-api-key flag not used")
+	}
+	if *testIndex == "" {
+		t.Skip("skipping test because -test-pinecone-index flag not used")
+	}
+
+	// We use a different namespace for each test to avoid confusion.
+	namespace := *testNamespace + "TestGenkit"
+
+	ctx := context.Background()
+
+	// Get information about the index.
+
+	client, err := NewClient(ctx, *testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	indexData, err := client.IndexData(ctx, *testIndex)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dim := indexData.Dimension
+
+	// Make two very similar vectors and one different vector.
+	// Arrange for a fake embedder to return those vector
+	// when provided with documents.
+
+	v1 := make([]float32, dim)
+	v2 := make([]float32, dim)
+	v3 := make([]float32, dim)
+	for i := range v1 {
+		v1[i] = float32(i)
+		v2[i] = float32(i)
+		v3[i] = float32(dim - i)
+	}
+	v2[0] = 1
+
+	d1 := genkit.DocumentFromText("hello1", nil)
+	d2 := genkit.DocumentFromText("hello2", nil)
+	d3 := genkit.DocumentFromText("goodbye", nil)
+
+	embedder := fakeembedder.New()
+	embedder.Register(d1, v1)
+	embedder.Register(d2, v2)
+	embedder.Register(d3, v3)
+
+	r, err := newRetriever(ctx, *testAPIKey, indexData.Host, embedder, nil, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	indexerOptions := &IndexerOptions{
+		Namespace: namespace,
+	}
+
+	indexerReq := &genkit.IndexerRequest{
+		Documents: []*genkit.Document{d1, d2, d3},
+		Options: indexerOptions,
+	}
+	err = r.Index(ctx, indexerReq)
+	if err != nil {
+		t.Fatalf("Index operation failed: %v", err)
+	}
+
+	defer func() {
+		idx, err := client.Index(ctx, indexData.Host)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var ids []string
+		addID := func(d *genkit.Document) {
+			id, err := docID(d)
+			if err != nil {
+				t.Error("can't get document ID")
+				return
+			}
+			ids = append(ids, id)
+		}
+		addID(d1)
+		addID(d2)
+		addID(d3)
+
+		if err := idx.DeleteByID(ctx, ids, namespace); err != nil {
+			t.Errorf("error deleting test vectors: %v", err)
+		}
+	}()
+
+	retrieverOptions := &RetrieverOptions{
+		Count:     2,
+		Namespace: namespace,
+	}
+
+	retrieverReq := &genkit.RetrieverRequest{
+		Document: d1,
+		Options:  retrieverOptions,
+	}
+	retrieverResp, err := r.Retrieve(ctx, retrieverReq)
+	if err != nil {
+		t.Fatalf("Retrieve operation failed: %v", err)
+	}
+
+	docs := retrieverResp.Documents
+	if len(docs) != 2 {
+		t.Errorf("got %d results, expected 2", len(docs))
+	}
+	for _, d := range docs {
+		text := d.Content[0].Text()
+		if !strings.HasPrefix(text, "hello") {
+			t.Errorf("returned doc text %q does not start with %q", text, "hello")
+		}
+	}
+}
