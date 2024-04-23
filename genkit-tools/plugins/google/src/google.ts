ad330450f108eb372e0611c0b4b855cddd350966
Anthony Barone <tonybaroneee@gmail.com>
2024-04-23 13:56:46 -0400

    Consolidate `@genkit-ai/tools-plugins` to `@genkit-ai/tools-common/plugin` (#165)
    Removes circular dependency.

--- genkit-tools/plugins/google/src/google.ts
+++ genkit-tools/plugins/google/src/google.ts
@@ -19,7 +19,7 @@ import {
   ToolPlugin,
   cliCommand,
   promptContinue,
-} from '@genkit-ai/tools-plugins';
+} from '@genkit-ai/tools-common/plugin';
 import * as clc from 'colorette';
 
 export const GoogleCloudTools: ToolPlugin = {
