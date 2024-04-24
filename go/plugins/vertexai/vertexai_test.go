b26a3bf615afdb84cca6af41eab8c3e1d52ca29f
Jonathan Amsterdam <jba@users.noreply.github.com>
2024-04-24 16:54:15 -0400

    [Go] add RunFlow and StreamFlow (#140)
    Add the RunFlow function, which runs a flow in the context of
    another flow.
    
    Also add the StreamFlow function, which runs a flow with streaming
    in the context of another flow.

--- go/plugins/vertexai/vertexai_test.go
+++ go/plugins/vertexai/vertexai_test.go
@@ -49,7 +49,7 @@ func TestGenerator(t *testing.T) {
 		},
 	}
 
-	resp, err := g.Generate(ctx, req)
+	resp, err := g.Generate(ctx, req, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
--- go/samples/flow-sample1/main.go
+++ go/samples/flow-sample1/main.go
@@ -12,23 +12,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This program can be manually tested like so:
+// Start the server listening on port 3100:
+//
+//	go run . &
+//
+// Tell it to run an action:
+//
+//	curl -d '{"key":"/flow/parent/parent", "input":{"start": {"input":null}}}'  http://localhost:3100/api/runAction
 package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/google/genkit/go/genkit"
 )
 
 func main() {
-	genkit.DefineFlow("basic", func(ctx context.Context, subject string, _ genkit.NoStream) (string, error) {
+	basic := genkit.DefineFlow("basic", func(ctx context.Context, subject string, _ genkit.NoStream) (string, error) {
 		foo, err := genkit.Run(ctx, "call-llm", func() (string, error) { return "subject: " + subject, nil })
 		if err != nil {
 			return "", err
 		}
 		return genkit.Run(ctx, "call-llm", func() (string, error) { return "foo: " + foo, nil })
 	})
+
+	genkit.DefineFlow("parent", func(ctx context.Context, _ struct{}, _ genkit.NoStream) (string, error) {
+		return genkit.RunFlow(ctx, basic, "foo")
+	})
+
+	type chunk struct {
+		Count int `json:"count"`
+	}
+
+	genkit.DefineFlow("streamy", func(ctx context.Context, count int, cb genkit.StreamingCallback[chunk]) (string, error) {
+		i := 0
+		if cb != nil {
+			for ; i < count; i++ {
+				if err := cb(ctx, chunk{i}); err != nil {
+					return "", err
+				}
+			}
+		}
+		return fmt.Sprintf("done: %d, streamed: %d times", count, i), nil
+	})
+
 	if err := genkit.StartDevServer(""); err != nil {
 		log.Fatal(err)
 	}
