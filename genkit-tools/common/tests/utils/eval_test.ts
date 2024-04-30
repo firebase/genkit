157ad15c97abbf4dd852a518ff5d67d402eebffc
ssbushi <66321939+ssbushi@users.noreply.github.com>
2024-04-30 09:41:05 -0400

    Refine tests using traces (#223)

--- genkit-tools/common/tests/utils/eval_test.ts
+++ genkit-tools/common/tests/utils/eval_test.ts
@@ -18,312 +18,36 @@ import { describe, expect, it, jest } from '@jest/globals';
 import * as configModule from '../../src/plugin/config';
 import { TraceData } from '../../src/types/trace';
 import { getEvalExtractors } from '../../src/utils/eval';
+import { MockTrace } from './trace';
 
 const CONTEXT_TEXTS = [
-  'are about 10 times larger, making them particularly difficultfor humans to ignore.',
+  'are about 10 times larger, making them particularly difficult for humans to ignore.',
   'they are very big animals',
 ];
-const MOCK_TRACE: TraceData = {
-  traceId: '7c273c22b219d077c6731a10d46b7d40',
-  displayName: 'dev-run-action-wrapper',
-  startTime: 1714059149480,
-  endTime: 1714059149486.6926,
-  spans: {
-    dfe3b782071ea308: {
-      spanId: 'dfe3b782071ea308',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '30506fdf0beccc15',
-      startTime: 1714059149481,
-      endTime: 1714059149481.1533,
-      attributes: {
-        'genkit:type': 'flowStep',
-        'genkit:name': 'step1',
-        'genkit:path': '/dev-run-action-wrapper/multiSteps/multiSteps/step1',
-        'genkit:metadata:flow:stepType': 'run',
-        'genkit:metadata:flow:stepName': 'step1',
-        'genkit:metadata:flow:resolvedStepName': 'step1',
-        'genkit:metadata:flow:state': 'run',
-        'genkit:output': '"Hello, Douglas Adams! step 1"',
-        'genkit:state': 'success',
-      },
-      displayName: 'step1',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    dd735a0ba4e4dafd: {
-      spanId: 'dd735a0ba4e4dafd',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '30506fdf0beccc15',
-      startTime: 1714059149481,
-      endTime: 1714059149481.3718,
-      attributes: {
-        'genkit:type': 'flowStep',
-        'genkit:name': 'step2',
-        'genkit:path': '/dev-run-action-wrapper/multiSteps/multiSteps/step2',
-        'genkit:metadata:flow:stepType': 'run',
-        'genkit:metadata:flow:stepName': 'step2',
-        'genkit:metadata:flow:resolvedStepName': 'step2',
-        'genkit:metadata:flow:state': 'run',
-        'genkit:input': '"Hello, Douglas Adams! step 1"',
-        'genkit:output': '"Hello, Douglas Adams! step 1 Faf "',
-        'genkit:state': 'success',
-      },
-      displayName: 'step2',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    b002dd6a0d92977d: {
-      spanId: 'b002dd6a0d92977d',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '30506fdf0beccc15',
-      startTime: 1714097043891,
-      endTime: 1714097044574.1538,
-      attributes: {
-        'genkit:type': 'action',
-        'genkit:name': 'devLocalVectorstore/pdfQA',
-        'genkit:path':
-          '/dev-run-action-wrapper/multiSteps/multiSteps/devLocalVectorstore/pdfQA',
-        'genkit:input':
-          '{"query":{"content":[{"text":"What are cats?"}]},"options":{"k":3}}',
-        'genkit:metadata:subtype': 'retriever',
-        'genkit:output':
-          '{"documents":[{"content":[{"text":"are about 10 times larger, making them particularly difficultfor humans to ignore."}]},{"content":[{"text":"they are very big animals"}]}]}',
-        'genkit:state': 'success',
-      },
-      displayName: 'devLocalVectorstore/pdfQA',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    '41b5ce70e453ae02': {
-      spanId: '41b5ce70e453ae02',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '30506fdf0beccc15',
-      startTime: 1714059149482,
-      endTime: 1714059149482.3955,
-      attributes: {
-        'genkit:type': 'flowStep',
-        'genkit:name': 'step3-array',
-        'genkit:path':
-          '/dev-run-action-wrapper/multiSteps/multiSteps/step3-array',
-        'genkit:metadata:flow:stepType': 'run',
-        'genkit:metadata:flow:stepName': 'step3-array',
-        'genkit:metadata:flow:resolvedStepName': 'step3-array',
-        'genkit:metadata:flow:state': 'run',
-        'genkit:output':
-          '["Hello, Douglas Adams! step 1 Faf ","Hello, Douglas Adams! step 1 Faf "]',
-        'genkit:state': 'success',
-      },
-      displayName: 'step3-array',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    '39e81338f055c25b': {
-      spanId: '39e81338f055c25b',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '30506fdf0beccc15',
-      startTime: 1714059149483,
-      endTime: 1714059149483.3872,
-      attributes: {
-        'genkit:type': 'flowStep',
-        'genkit:name': 'step4-num',
-        'genkit:path':
-          '/dev-run-action-wrapper/multiSteps/multiSteps/step4-num',
-        'genkit:metadata:flow:stepType': 'run',
-        'genkit:metadata:flow:stepName': 'step4-num',
-        'genkit:metadata:flow:resolvedStepName': 'step4-num',
-        'genkit:metadata:flow:state': 'run',
-        'genkit:output':
-          '"Hello, Douglas Adams! step 1 Faf -()-Hello, Douglas Adams! step 1 Faf "',
-        'genkit:state': 'success',
-      },
-      displayName: 'step4-num',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    '30506fdf0beccc15': {
-      spanId: '30506fdf0beccc15',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: 'eec307f9311c5617',
-      startTime: 1714059149481,
-      endTime: 1714059149484.9653,
-      attributes: {
-        'genkit:type': 'flow',
-        'genkit:name': 'multiSteps',
-        'genkit:isRoot': true,
-        'genkit:path': '/dev-run-action-wrapper/multiSteps/multiSteps',
-        'genkit:metadata:flow:execution': '0',
-        'genkit:metadata:flow:name': 'multiSteps',
-        'genkit:metadata:flow:id': 'c68dd3b2-6bb4-44ef-907d-cef5720d6b6b',
-        'genkit:metadata:flow:dispatchType': 'start',
-        'genkit:metadata:flow:state': 'done',
-        'genkit:input': '"Douglas Adams"',
-        'genkit:output': '42',
-        'genkit:state': 'success',
-      },
-      displayName: 'multiSteps',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    eec307f9311c5617: {
-      spanId: 'eec307f9311c5617',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      parentSpanId: '98e7d0c2f5e90f4e',
-      startTime: 1714059149480,
-      endTime: 1714059149485.578,
-      attributes: {
-        'genkit:type': 'action',
-        'genkit:name': 'multiSteps',
-        'genkit:path': '/dev-run-action-wrapper/multiSteps',
-        'genkit:input': '{"start":{"input":"Douglas Adams"}}',
-        'genkit:metadata:flow:wrapperAction': 'true',
-        'genkit:output':
-          '{"flowId":"c68dd3b2-6bb4-44ef-907d-cef5720d6b6b","name":"multiSteps","startTime":1714059149480,"input":"Douglas Adams","cache":{"step1":{"value":"Hello, Douglas Adams! step 1"},"step2":{"value":"Hello, Douglas Adams! step 1 Faf "},"step3-array":{"value":["Hello, Douglas Adams! step 1 Faf ","Hello, Douglas Adams! step 1 Faf "]},"step4-num":{"value":"Hello, Douglas Adams! step 1 Faf -()-Hello, Douglas Adams! step 1 Faf "}},"eventsTriggered":{},"blockedOnStep":null,"executions":[{"startTime":1714059149481,"traceIds":["7c273c22b219d077c6731a10d46b7d40"]}],"operation":{"name":"c68dd3b2-6bb4-44ef-907d-cef5720d6b6b","done":true,"result":{"response":42}},"traceContext":"{\\"traceId\\":\\"7c273c22b219d077c6731a10d46b7d40\\",\\"spanId\\":\\"30506fdf0beccc15\\",\\"traceFlags\\":1}"}',
-        'genkit:state': 'success',
-      },
-      displayName: 'multiSteps',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-    '98e7d0c2f5e90f4e': {
-      spanId: '98e7d0c2f5e90f4e',
-      traceId: '7c273c22b219d077c6731a10d46b7d40',
-      startTime: 1714059149480,
-      endTime: 1714059149486.6926,
-      attributes: {
-        'genkit:name': 'dev-run-action-wrapper',
-        'genkit:isRoot': true,
-        'genkit:path': '/dev-run-action-wrapper',
-        'genkit:metadata:genkit-dev-internal': 'true',
-        'genkit:state': 'success',
-      },
-      displayName: 'dev-run-action-wrapper',
-      links: [],
-      instrumentationLibrary: {
-        name: 'genkit-tracer',
-        version: 'v1',
-      },
-      spanKind: 'INTERNAL',
-      sameProcessAsParentSpan: {
-        value: true,
-      },
-      status: {
-        code: 0,
-      },
-      timeEvents: {
-        timeEvent: [],
-      },
-    },
-  },
-};
 
 describe('eval utils', () => {
   it('returns default extractors when no config provided', async () => {
     const spy = jest.spyOn(configModule, 'findToolsConfig');
     spy.mockReturnValue(Promise.resolve(null));
+    const trace = new MockTrace('My input', 'My output')
+      .addSpan({
+        stepName: 'retrieverStep',
+        spanType: 'action',
+        retrieverConfig: {
+          query: 'What are cats?',
+          text: CONTEXT_TEXTS,
+        },
+      })
+      .getTrace();
 
     const extractors = await getEvalExtractors('multiSteps');
 
     expect(Object.keys(extractors).sort()).toEqual(
       ['input', 'output', 'context'].sort()
     );
-    expect(extractors.input(MOCK_TRACE)).toEqual(
-      JSON.stringify('Douglas Adams')
-    );
-    expect(extractors.output(MOCK_TRACE)).toEqual(JSON.stringify(42));
-    expect(extractors.context(MOCK_TRACE)).toEqual(
-      JSON.stringify(CONTEXT_TEXTS)
-    );
+    expect(extractors.input(trace)).toEqual(JSON.stringify('My input'));
+    expect(extractors.output(trace)).toEqual(JSON.stringify('My output'));
+    expect(extractors.context(trace)).toEqual(JSON.stringify(CONTEXT_TEXTS));
   });
 
   it('returns custom extractors by stepName', async () => {
@@ -339,18 +63,30 @@ describe('eval utils', () => {
     };
     const spy = jest.spyOn(configModule, 'findToolsConfig');
     spy.mockReturnValue(Promise.resolve(config));
+    const trace = new MockTrace('My input', 42)
+      .addSpan({
+        stepName: 'retrieverStep',
+        spanType: 'action',
+        retrieverConfig: {
+          query: 'What are cats?',
+          text: CONTEXT_TEXTS,
+        },
+      })
+      .addSpan({
+        stepName: 'step1',
+        spanType: 'flowStep',
+        input: 'step-input',
+        output: { out: 'my-object-output' },
+      })
+      .getTrace();
 
     const extractors = await getEvalExtractors('multiSteps');
 
-    expect(extractors.input(MOCK_TRACE)).toEqual(
-      JSON.stringify('Douglas Adams')
+    expect(extractors.input(trace)).toEqual(JSON.stringify('My input'));
+    expect(extractors.output(trace)).toEqual(
+      JSON.stringify({ out: 'my-object-output' })
     );
-    expect(extractors.output(MOCK_TRACE)).toEqual(
-      JSON.stringify('Hello, Douglas Adams! step 1')
-    );
-    expect(extractors.context(MOCK_TRACE)).toEqual(
-      JSON.stringify(CONTEXT_TEXTS)
-    );
+    expect(extractors.context(trace)).toEqual(JSON.stringify(CONTEXT_TEXTS));
   });
 
   it('returns custom extractors by stepSelector', async () => {
@@ -367,20 +103,35 @@ describe('eval utils', () => {
     };
     const spy = jest.spyOn(configModule, 'findToolsConfig');
     spy.mockReturnValue(Promise.resolve(config));
+    const trace = new MockTrace('My input', 42)
+      .addSpan({
+        stepName: 'retrieverStep',
+        spanType: 'action',
+        retrieverConfig: {
+          query: 'What are cats?',
+          text: CONTEXT_TEXTS,
+        },
+      })
+      .addSpan({
+        stepName: 'step2',
+        spanType: 'flowStep',
+        input: 'step2-input',
+        output: 'step2-output',
+      })
+      .addSpan({
+        stepName: 'step3-array',
+        spanType: 'flowStep',
+        input: 'step3-input',
+        output: ['Hello', 'World'],
+      })
+      .getTrace();
 
     const extractors = await getEvalExtractors('multiSteps');
 
-    expect(extractors.input(MOCK_TRACE)).toEqual(
-      JSON.stringify('Douglas Adams')
-    );
-    expect(extractors.output(MOCK_TRACE)).toEqual(
-      JSON.stringify('Hello, Douglas Adams! step 1')
-    );
-    expect(extractors.context(MOCK_TRACE)).toEqual(
-      JSON.stringify([
-        'Hello, Douglas Adams! step 1 Faf ',
-        'Hello, Douglas Adams! step 1 Faf ',
-      ])
+    expect(extractors.input(trace)).toEqual(JSON.stringify('My input'));
+    expect(extractors.output(trace)).toEqual(JSON.stringify('step2-input'));
+    expect(extractors.context(trace)).toEqual(
+      JSON.stringify(['Hello', 'World'])
     );
   });
 
@@ -417,20 +168,37 @@ describe('eval utils', () => {
     };
     const spy = jest.spyOn(configModule, 'findToolsConfig');
     spy.mockReturnValue(Promise.resolve(config));
+    const trace = new MockTrace('My input', 42)
+      .addSpan({
+        stepName: 'retrieverStep',
+        spanType: 'action',
+        retrieverConfig: {
+          query: 'What are cats?',
+          text: CONTEXT_TEXTS,
+        },
+      })
+      .addSpan({
+        stepName: 'step2',
+        spanType: 'flowStep',
+        input: 'step2-input',
+        output: 'step2-output',
+      })
+      .addSpan({
+        stepName: 'step3-array',
+        spanType: 'flowStep',
+        input: 'step3-input',
+        output: ['Hello', 'World'],
+      })
+      .getTrace();
 
     const extractors = await getEvalExtractors('multiSteps');
 
-    expect(extractors.input(MOCK_TRACE)).toEqual(
-      JSON.stringify(['Douglas Adams TEST TEST TEST'])
+    expect(extractors.input(trace)).toEqual(
+      JSON.stringify(['My input TEST TEST TEST'])
     );
-    expect(extractors.output(MOCK_TRACE)).toEqual(
-      JSON.stringify('Hello, Douglas Adams! step 1')
-    );
-    expect(extractors.context(MOCK_TRACE)).toEqual(
-      JSON.stringify([
-        'Hello, Douglas Adams! step 1 Faf ',
-        'Hello, Douglas Adams! step 1 Faf ',
-      ])
+    expect(extractors.output(trace)).toEqual(JSON.stringify('step2-input'));
+    expect(extractors.context(trace)).toEqual(
+      JSON.stringify(['Hello', 'World'])
     );
   });
 });
