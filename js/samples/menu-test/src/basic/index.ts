47bf8dc55edcf6dc7e7becd21fd2367433a9c9cb
Chris Chestnut <cchestnut@google.com>
2024-05-07 15:38:07 +0000

    wip - creating sample menu-test directory to test changes samples in isolation

---
+++ js/samples/menu-test/src/basic/index.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { configureGenkit } from '@genkit-ai/core';
+import { generate } from '@genkit-ai/ai';
+import { googleAI, geminiPro } from '@genkit-ai/googleai';
+import { defineFlow } from '@genkit-ai/flow';
+import * as z from 'zod';
+
+export default configureGenkit({
+    plugins: [googleAI()],
+    logLevel: 'debug',
+    enableTracingAndMetrics: true,
+  });
+
+  export const menuQAFlow = defineFlow(
+    {
+      name: 'menuQAFlow',
+      inputSchema: z.string(),
+      outputSchema: z.string(),
+    },
+    async (subject) => {
+      const llmResponse = await generate({
+        prompt: `Tell me a long joke about ${subject}`,
+        model: geminiPro,
+        config: {
+          temperature: 1,
+        },
+      });
+  
+      return llmResponse.text();
+    }
+  );
