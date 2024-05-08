ca97da45c84cf752495d5fc8ceec01388584ad38
Pavel Jbanov <pavelj@google.com>
2024-05-08 16:22:14 -0400

    [Go] adding metadata to models, etc. (#71)

--- go/plugins/googleai/googleai.go
+++ go/plugins/googleai/googleai.go
@@ -204,12 +204,16 @@ func Init(ctx context.Context, model, apiKey string) error {
 		return err
 	}
 	ai.RegisterEmbedder("google-genai", e)
-
 	g, err := NewGenerator(ctx, model, apiKey)
 	if err != nil {
 		return err
 	}
-	ai.RegisterGenerator("google-genai", g)
+	ai.RegisterGenerator("google-genai", model, &ai.GeneratorMetadata{
+		Label: "Google AI - " + model,
+		Supports: ai.GeneratorCapabilities{
+			Multiturn: true,
+		},
+	}, g)
 
 	return nil
 }
