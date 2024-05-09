ab6b18bdea5c6d8cc39e0238232aee9370b7a885
Chris Chestnut <cchestnut@google.com>
2024-05-09 15:32:35 +0000

    wip looking for feedback

---
+++ js/samples/menu-example/generation/src/index.ts
@@ -0,0 +1,102 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This sample is referenced by the genkit docs. Changes should be made to
+// both.
+import { generate } from '@genkit-ai/ai';
+import { configureGenkit } from '@genkit-ai/core';
+import { defineFlow, startFlowsServer } from '@genkit-ai/flow';
+import { geminiPro, geminiProVision, googleAI } from '@genkit-ai/googleai';
+import * as z from 'zod';
+import { MessageData } from '@genkit-ai/ai/model';
+
+configureGenkit({
+  plugins: [
+    googleAI(),
+  ],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true,
+});
+
+export const menuSuggestionFlow = defineFlow(
+  {
+    name: 'menuSuggestionFlow',
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (subject) => {
+    const llmResponse = await generate({
+      prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
+      model: geminiPro,
+      config: {
+        temperature: 1,
+      },
+    });
+
+    return llmResponse.text();
+  }
+);
+
+let chatHistory: MessageData[];
+
+export const menuHistoryFlow = defineFlow(
+  {
+    name: 'menuHistoryFlow',
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+  },
+  async (subject) => {
+    let history = chatHistory;
+    const llmResponse = await generate({
+      prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
+      history,
+      model: geminiPro,
+      config: {
+        temperature: 1,
+      },
+    });
+    chatHistory = llmResponse.toHistory();
+    return llmResponse.text();
+  }
+)
+
+export const menuImageFlow = defineFlow({
+  name: 'menuImageFlow',
+  inputSchema: z.object({ imageUrl: z.string(), subject: z.string()}),
+  outputSchema: z.string(),
+},
+  async (input) => {
+    const visionResponse = await generate({
+      model: geminiProVision,
+      prompt: [
+        { text: `Extract _all_ of the text, in order, 
+          from the following image of a restaurant menu.`},
+        { media: {url: input.imageUrl, contentType: 'image/jpeg'}}
+      ]
+    });
+    const imageDescription = visionResponse.text();
+
+    const response = await generate({
+      model: geminiPro,
+      prompt: `Here is the text of today's menu: ${imageDescription} 
+      Theme the items to match the restaurant's ${input.subject} theme.`
+    });
+
+    return response.text();
+  }
+)
+
+startFlowsServer();
