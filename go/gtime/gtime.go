299dbb802ace2f7d28d7bfeda4d8b7a4900e971a
Jonathan Amsterdam <jba@google.com>
2024-05-21 17:29:41 -0400

    gtime doc

--- go/gtime/gtime.go
+++ go/gtime/gtime.go
@@ -20,11 +20,13 @@ import "time"
 // Milliseconds represents a time as the number of milliseconds since the Unix epoch.
 type Milliseconds float64
 
+// ToMilliseconds converts a time.Time to a Milliseconds.
 func ToMilliseconds(t time.Time) Milliseconds {
 	nsec := t.UnixNano()
 	return Milliseconds(float64(nsec) / 1e6)
 }
 
+// Time converts a Milliseconds to a time.Time.
 func (m Milliseconds) Time() time.Time {
 	sec := int64(m / 1e3)
 	nsec := int64((float64(m) - float64(sec*1e3)) * 1e6)
