aacd71cf91e1451387df43380404a35a05619137
Jonathan Amsterdam <jba@google.com>
2024-05-23 13:23:43 -0400

    remove junk comment

--- go/genkit/servers.go
+++ go/genkit/servers.go
@@ -229,29 +229,6 @@ type listFlowStatesResult struct {
 	ContinuationToken string       `json:"continuationToken"`
 }
 
-// Copyright 2024 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// package genkit
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io"
-// 	"net/http"
-// )
-
 // StartProdServer starts a production server listening at the given address.
 // The Server has a route for each defined flow.
 // If addr is "", it uses the value of the environment variable PORT
