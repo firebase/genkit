8e6d97fb001aa39f39410ef89053549f4ec0ae2a
Jonathan Amsterdam <jba@google.com>
2024-07-06 08:39:32 -0400

    [Go] e2e test for reflection API
    Write a Go test that uses the yaml file for reflection API tests
    from #549.
    
    Currently, the api/actions test fails because the schemas
    are very different between Go and JS.

---
+++ go/tests/test_app/main.go
@@ -0,0 +1,58 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/firebase/genkit/go/ai"
+	"github.com/firebase/genkit/go/genkit"
+)
+
+func main() {
+	model := ai.DefineModel("", "customReflector", nil, echo)
+	genkit.DefineFlow("testFlow", func(ctx context.Context, in string) (string, error) {
+		res, err := model.Generate(ctx, ai.NewGenerateRequest(nil, ai.NewUserTextMessage(in)), nil)
+		if err != nil {
+			return "", err
+		}
+		// got := res.Candidates[0].Message.Content[0]
+		_ = res
+		// fmt.Print
+		return "TBD", nil
+	})
+	if err := genkit.Init(context.Background(), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func echo(ctx context.Context, req *ai.GenerateRequest, cb func(context.Context, *ai.GenerateResponseChunk) error) (*ai.GenerateResponse, error) {
+	jsonBytes, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	return &ai.GenerateResponse{
+		Candidates: []*ai.Candidate{{
+			Index:        0,
+			FinishReason: "stop",
+			Message: &ai.Message{
+				Role:    "model",
+				Content: []*ai.Part{ai.NewTextPart(string(jsonBytes))},
+			},
+		}},
+	}, nil
+}
