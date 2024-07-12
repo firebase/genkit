61a83ca0e009fe9b6bbd9d563f7652a6fda1c74f
Pavel Jbanov <pavelj@google.com>
2024-07-12 14:06:37 -0400

    chore: use auto-generated version for GENKIT_VERSION (#598)

--- js/core/src/index.ts
+++ js/core/src/index.ts
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
-export const GENKIT_VERSION = '0.5.5';
+import { version } from './__codegen/version.js';
+
+export const GENKIT_VERSION = version;
 export const GENKIT_CLIENT_HEADER = `genkit-node/${GENKIT_VERSION} gl-node/${process.versions.node}`;
 
 export * from './action.js';
