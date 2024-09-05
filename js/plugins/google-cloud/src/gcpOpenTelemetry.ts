00e863dabeb379c5648bffbf4801d5bfe5127708
Konstantin Mandrika <kmandrika@google.com>
2024-09-05 18:04:51 -0400

    Move telemetry & logging into the GCP plugin

--- js/plugins/google-cloud/src/gcpOpenTelemetry.ts
+++ js/plugins/google-cloud/src/gcpOpenTelemetry.ts
@@ -20,7 +20,11 @@ import { TraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter'
 import { GcpDetectorSync } from '@google-cloud/opentelemetry-resource-util';
 import { Span, SpanStatusCode, TraceFlags } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { ExportResult } from '@opentelemetry/core';
+import {
+  ExportResult,
+  hrTimeDuration,
+  hrTimeToMilliseconds,
+} from '@opentelemetry/core';
 import { Instrumentation } from '@opentelemetry/instrumentation';
 import { PinoInstrumentation } from '@opentelemetry/instrumentation-pino';
 import { WinstonInstrumentation } from '@opentelemetry/instrumentation-winston';
@@ -42,7 +46,14 @@ import {
   ReadableSpan,
   SpanExporter,
 } from '@opentelemetry/sdk-trace-base';
+
+import { extractErrorName } from './utils';
+
+import { PathMetadata } from '@genkit-ai/core/tracing';
 import { PluginOptions } from './index.js';
+import { actionTelemetry } from './telemetry/action.js';
+import { flowsTelemetry } from './telemetry/flow.js';
+import { generateTelemetry } from './telemetry/generate.js';
 
 let metricExporter: PushMetricExporter;
 let spanProcessor: BatchSpanProcessor;
@@ -203,12 +214,68 @@ class AdjustingTraceExporter implements SpanExporter {
   }
 
   private adjust(spans: ReadableSpan[]): ReadableSpan[] {
+    const allPaths = spans
+      .filter((span) => span.attributes['genkit:path'])
+      .map(
+        (span) =>
+          ({
+            path: span.attributes['genkit:path'] as string,
+            status:
+              (span.attributes['genkit:state'] as string) === 'error'
+                ? 'failure'
+                : 'success',
+            error: extractErrorName(span.events),
+            latency: hrTimeToMilliseconds(
+              hrTimeDuration(span.startTime, span.endTime)
+            ),
+          }) as PathMetadata
+      );
+
+    const allLeafPaths = new Set<PathMetadata>(
+      allPaths.filter((leafPath) =>
+        allPaths.every(
+          (path) =>
+            path.path === leafPath.path ||
+            !path.path.startsWith(leafPath.path) ||
+            (path.path.startsWith(leafPath.path) &&
+              path.status !== leafPath.status)
+        )
+      )
+    );
+
     return spans.map((span) => {
+      this.tickTelemetry(span, allLeafPaths);
+
       span = this.redactPii(span);
       span = this.markErrorSpanAsError(span);
       span = this.normalizeLabels(span);
       return span;
     });
+  }
+
+  private tickTelemetry(span: ReadableSpan, paths: Set<PathMetadata>) {
+    const attributes = span.attributes;
+
+    if (!Object.keys(attributes).includes('genkit:type')) {
+      return;
+    }
+
+    const type = attributes['genkit:type'] as string;
+    const subtype = attributes['genkit:metadata:subtype'] as string;
+
+    if (type === 'flow' || type === 'flowStep') {
+      flowsTelemetry.tick(span, paths);
+      return;
+    }
+
+    if (type === 'action' && subtype === 'model') {
+      generateTelemetry.tick(span, paths);
+      return;
+    }
+
+    if (type === 'action') {
+      actionTelemetry.tick(span, paths);
+    }
   }
 
   private redactPii(span: ReadableSpan): ReadableSpan {
---
+++ js/plugins/google-cloud/src/utils.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { TimedEvent } from '@opentelemetry/sdk-trace-base';
+
+export function extractOuterFlowNameFromPath(path: string) {
+  if (!path || path === '<unknown>') {
+    return '<unknown>';
+  }
+
+  const flowName = path.match('/{(.+),t:flow}+');
+  return flowName ? flowName[1] : '<unknown>';
+}
+
+export function extractErrorName(events: TimedEvent[]): string | undefined {
+  return events
+    .filter((event) => event.name === 'exception')
+    .map((event) => {
+      const attributes = event.attributes;
+      return attributes
+        ? (attributes['exception.type'] as string)
+        : '<unknown>';
+    })
+    .at(0);
+}
+
+export function extractErrorMessage(events: TimedEvent[]): string | undefined {
+  return events
+    .filter((event) => event.name === 'exception')
+    .map((event) => {
+      const attributes = event.attributes;
+      return attributes
+        ? (attributes['exception.message'] as string)
+        : '<unknown>';
+    })
+    .at(0);
+}
+
+export function extractErrorStack(events: TimedEvent[]): string | undefined {
+  return events
+    .filter((event) => event.name === 'exception')
+    .map((event) => {
+      const attributes = event.attributes;
+      return attributes
+        ? (attributes['exception.stacktrace'] as string)
+        : '<unknown>';
+    })
+    .at(0);
+}
