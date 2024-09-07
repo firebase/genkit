bef5c3819a448c525f588f131a5b3b32a13e568a
Pavel Jbanov <pavelj@google.com>
2024-09-07 16:47:17 -0400

    chore: added model tester library and testapp

---
+++ js/ai/src/testing/model-tester.ts
@@ -0,0 +1,214 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { lookupAction } from '@genkit-ai/core/registry';
+import { runInNewSpan } from '@genkit-ai/core/tracing';
+import assert from 'node:assert';
+import z from 'zod';
+import { generate } from '../generate';
+import { ModelAction } from '../model';
+import { defineTool } from '../tool';
+
+const tests: Record<string, TestCase> = {
+  'basic hi': async (model: string) => {
+    const response = await generate({
+      model,
+      prompt: 'just say "Hi", literally',
+    });
+
+    const got = response.text().trim();
+    assert.match(got, /Hi/i);
+  },
+  multimodal: async (model: string) => {
+    const resolvedModel = (await lookupAction(
+      `/model/${model}`
+    )) as ModelAction;
+    if (!resolvedModel.__action.metadata?.model.supports?.media) {
+      skip();
+    }
+    const response = await generate({
+      model,
+      prompt: [
+        {
+          media: {
+            url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9TpSoVETOIOGSoulgQFXHUKhShQqgVWnUwufRDaNKQtLg4Cq4FBz8Wqw4uzro6uAqC4AeIs4OToouU+L+k0CLGg+N+vLv3uHsHCLUi0+22MUA3ylYyHpPSmRUp9IpOhCCiFyMKs81ZWU7Ad3zdI8DXuyjP8j/35+jWsjYDAhLxDDOtMvE68dRm2eS8TyyygqIRnxOPWnRB4keuqx6/cc67LPBM0Uol54hFYinfwmoLs4KlE08SRzTdoHwh7bHGeYuzXqywxj35C8NZY3mJ6zQHEccCFiFDgooKNlBEGVFaDVJsJGk/5uMfcP0yuVRybYCRYx4l6FBcP/gf/O7Wzk2Me0nhGND+4jgfQ0BoF6hXHef72HHqJ0DwGbgymv5SDZj+JL3a1CJHQM82cHHd1NQ94HIH6H8yFUtxpSBNIZcD3s/omzJA3y3Qter11tjH6QOQoq4SN8DBITCcp+w1n3d3tPb275lGfz9aC3Kd0jYiSQAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+gJBxQRO1/5qB8AAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAsUlEQVQoz61SMQqEMBDcO5SYToUE/IBPyRMCftAH+INUviApUwYjNkKCVcTiQK7IHSw45czODrMswCOQUkopEQZjzDiOWemdZfu+b5oGYYgx1nWNMPwB2vACAK01Y4wQ8qGqqirL8jzPlNI9t64r55wQUgBA27be+xDCfaJhGJxzSqnv3UKIn7ne+2VZEB2stZRSRLN93+d5RiRs28Y5RySEEI7jyEpFlp2mqeu6Zx75ApQwPdsIcq0ZAAAAAElFTkSuQmCC',
+          },
+        },
+        {
+          text: 'what math operation is this? plus, minus, multiply or divide?',
+        },
+      ],
+    });
+
+    const want = '';
+    const got = response.text().trim();
+    assert.match(got, /plus/i);
+  },
+  history: async (model: string) => {
+    const resolvedModel = (await lookupAction(
+      `/model/${model}`
+    )) as ModelAction;
+    if (!resolvedModel.__action.metadata?.model.supports?.multiturn) {
+      skip();
+    }
+    const response1 = await generate({
+      model,
+      prompt: 'My name is Glorb',
+    });
+    const response = await generate({
+      model,
+      prompt: "What' my name?",
+      history: response1.toHistory(),
+    });
+
+    const got = response.text().trim();
+    assert.match(got, /Glorb/);
+  },
+  'system prompt': async (model: string) => {
+    const response = await generate({
+      model,
+      prompt: 'Hi',
+      history: [
+        {
+          role: 'system',
+          content: [
+            {
+              text: 'If the user says "Hi", just say "Bye" ',
+            },
+          ],
+        },
+      ],
+    });
+
+    const want = 'Bye';
+    const got = response.text().trim();
+    assert.equal(got, want);
+  },
+  'structured output': async (model: string) => {
+    const response = await generate({
+      model,
+      prompt: 'extract data as json from: Jack the lumberjack',
+      output: {
+        format: 'json',
+        schema: z.object({
+          name: z.string(),
+          occupation: z.string(),
+        }),
+      },
+    });
+
+    const want = {
+      name: 'Jack',
+      occupation: 'lumberjack',
+    };
+    const got = response.output();
+    assert.deepEqual(want, got);
+  },
+  'tool calling': async (model: string) => {
+    const resolvedModel = (await lookupAction(
+      `/model/${model}`
+    )) as ModelAction;
+    if (!resolvedModel.__action.metadata?.model.supports?.tools) {
+      skip();
+    }
+
+    const response = await generate({
+      model,
+      prompt: 'what is a gablorken of 2 over 3.5? use provided tool',
+      tools: [gablorkenTool],
+    });
+
+    const got = response.text().trim();
+    assert.match(got, /13.657/);
+  },
+};
+
+type TestReport = {
+  description: string;
+  models: {
+    name: string;
+    passed: boolean;
+    skipped?: boolean;
+    error?: {
+      message: string;
+      stack?: string;
+    };
+  }[];
+}[];
+
+type TestCase = (model: string) => void;
+
+const gablorkenTool = defineTool(
+  {
+    name: 'gablorkenTool',
+    description: 'use when need to calculate a gablorken',
+    inputSchema: z.object({
+      value: z.number(),
+      over: z.number(),
+    }),
+    outputSchema: z.number(),
+  },
+  async (input) => {
+    return Math.pow(input.value, input.over) + 1.407;
+  }
+);
+
+export async function testModels(models: string[]): Promise<TestReport> {
+  return await runInNewSpan({ metadata: { name: 'testModels' } }, async () => {
+    const report: TestReport = [];
+    for (const test of Object.keys(tests)) {
+      await runInNewSpan({ metadata: { name: test } }, async () => {
+        report.push({
+          description: test,
+          models: [],
+        });
+        const caseReport = report[report.length - 1];
+        for (const model of models) {
+          caseReport.models.push({
+            name: model,
+            passed: true, // optimistically
+          });
+          const modelReport = caseReport.models[caseReport.models.length - 1];
+          try {
+            await tests[test](model);
+          } catch (e) {
+            modelReport.passed = false;
+            if (e instanceof SkipTestError) {
+              modelReport.skipped = true;
+            } else if (e instanceof Error) {
+              modelReport.error = {
+                message: e.message,
+                stack: e.stack,
+              };
+            } else {
+              modelReport.error = {
+                message: `${e}`,
+              };
+            }
+          }
+        }
+      });
+    }
+
+    return report;
+  });
+}
+
+class SkipTestError extends Error {}
+
+function skip() {
+  throw new SkipTestError();
+}
---
+++ js/testapps/model-tester/src/index.ts
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { testModels } from '@genkit-ai/ai/testing';
+import { configureGenkit } from '@genkit-ai/core';
+import { googleAI } from '@genkit-ai/googleai';
+import { claude3Sonnet, llama31, vertexAI } from '@genkit-ai/vertexai';
+import * as clc from 'colorette';
+import { ollama } from 'genkitx-ollama';
+
+configureGenkit({
+  plugins: [
+    googleAI(),
+    vertexAI({
+      location: 'us-central1',
+      modelGarden: {
+        models: [claude3Sonnet, llama31],
+      },
+    }),
+    ollama({
+      models: [
+        {
+          name: 'gemma2',
+          type: 'chat',
+        },
+      ],
+      serverAddress: 'http://127.0.0.1:11434', // default local address
+    }),
+  ],
+  enableTracingAndMetrics: true,
+  logLevel: 'debug',
+});
+
+testModels([
+  'googleai/gemini-1.5-pro-latest',
+  'googleai/gemini-1.5-flash-latest',
+  'vertexai/gemini-1.5-pro',
+  'vertexai/gemini-1.5-flash',
+  'vertexai/claude-3-sonnet',
+  'vertexai/llama-3.1',
+  'ollama/gemma2',
+]).then((r) => {
+  let failed = false;
+  for (const test of r) {
+    console.log(`${clc.bold('Test:')} ${test.description}`);
+    for (const model of test.models) {
+      if (model.passed) {
+        console.log(
+          `  - ${clc.bold(clc.green('PASSED'))}: ${clc.bold(model.name)}`
+        );
+      } else if (model.skipped) {
+        console.log(
+          `  - ${clc.bold(clc.yellow('SKIPPED'))}: ${clc.bold(model.name)}`
+        );
+      } else {
+        failed = true;
+        console.log(
+          `  - ${clc.bold(clc.red('FAILED'))}: ${clc.bold(model.name)}`
+        );
+        if (model.error?.message) {
+          console.log(prefixText(model.error?.message, '    '));
+        }
+        if (model.error?.stack) {
+          console.log(prefixText(model.error?.stack, '    '));
+        }
+      }
+    }
+  }
+  if (failed) {
+    process.exitCode = 1;
+  }
+});
+
+function prefixText(txt: string, prefix: string) {
+  return txt
+    .split('\n')
+    .map((t) => prefix + t)
+    .join('\n');
+}
