fe01532a3c7c39d807b8f1ab5758f4d85ab67bb2
Alex Pascal <apascal07@gmail.com>
2024-09-19 10:02:59 -0700

    Fixed remaining tests in `ai`.

--- js/ai/tests/prompt/prompt_test.ts
+++ js/ai/tests/prompt/prompt_test.ts
@@ -14,43 +14,47 @@
  * limitations under the License.
  */
 
+import { Registry, runWithRegistry } from '@genkit-ai/core/registry';
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
 import * as z from 'zod';
 import { definePrompt, renderPrompt } from '../../src/prompt.ts';
 
 describe('prompt', () => {
+  let registry = new Registry();
   describe('render()', () => {
-    it('respects output schema in the definition', async () => {
-      const schema1 = z.object({
-        puppyName: z.string({ description: 'A cute name for a puppy' }),
-      });
-      const prompt1 = definePrompt(
-        {
-          name: 'prompt1',
-          inputSchema: z.string({ description: 'Dog breed' }),
-        },
-        async (breed) => {
-          return {
-            messages: [
-              {
-                role: 'user',
-                content: [{ text: `Pick a name for a ${breed} puppy` }],
+    runWithRegistry(registry, () => {
+      it('respects output schema in the definition', async () => {
+        const schema1 = z.object({
+          puppyName: z.string({ description: 'A cute name for a puppy' }),
+        });
+        const prompt1 = definePrompt(
+          {
+            name: 'prompt1',
+            inputSchema: z.string({ description: 'Dog breed' }),
+          },
+          async (breed) => {
+            return {
+              messages: [
+                {
+                  role: 'user',
+                  content: [{ text: `Pick a name for a ${breed} puppy` }],
+                },
+              ],
+              output: {
+                format: 'json',
+                schema: schema1,
               },
-            ],
-            output: {
-              format: 'json',
-              schema: schema1,
-            },
-          };
-        }
-      );
-      const generateRequest = await renderPrompt({
-        prompt: prompt1,
-        input: 'poodle',
-        model: 'geminiPro',
+            };
+          }
+        );
+        const generateRequest = await renderPrompt({
+          prompt: prompt1,
+          input: 'poodle',
+          model: 'geminiPro',
+        });
+        assert.equal(generateRequest.output?.schema, schema1);
       });
-      assert.equal(generateRequest.output?.schema, schema1);
     });
   });
 });
