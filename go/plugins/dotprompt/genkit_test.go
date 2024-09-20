96f5460365a6123552f9b48b9af1425e9da25445
Dhia Ayachi <dhia.ayachi@gmail.com>
2024-09-20 10:44:55 -0400

    refactor test to use subtests

--- go/plugins/dotprompt/genkit_test.go
+++ go/plugins/dotprompt/genkit_test.go
@@ -43,47 +43,31 @@ func testGenerate(ctx context.Context, req *ai.GenerateRequest, cb func(context.
 
 func TestExecute(t *testing.T) {
 	testModel := ai.DefineModel("test", "test", nil, testGenerate)
-	p, err := New("TestExecute", "TestExecute", Config{Model: testModel})
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := p.Generate(context.Background(), &PromptRequest{}, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(resp.Candidates) != 1 {
-		t.Errorf("got %d candidates, want 1", len(resp.Candidates))
-		if len(resp.Candidates) < 1 {
-			t.FailNow()
+	t.Run("Model", func(t *testing.T) {
+		p, err := New("TestExecute", "TestExecute", Config{Model: testModel})
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
-	msg := resp.Candidates[0].Message
-	if msg == nil {
-		t.Fatal("response has candidate with no message")
-	}
-	if len(msg.Content) != 1 {
-		t.Errorf("got %d message parts, want 1", len(msg.Content))
-		if len(msg.Content) < 1 {
-			t.FailNow()
+		resp, err := p.Generate(context.Background(), &PromptRequest{}, nil)
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
-	got := msg.Content[0].Text
-	want := `AI reply to "TestExecute"`
-	if got != want {
-		t.Errorf("fake model replied with %q, want %q", got, want)
-	}
+		assertResponse(t, resp)
+	})
+	t.Run("ModelName", func(t *testing.T) {
+		p, err := New("TestExecute", "TestExecute", Config{ModelName: "test/test"})
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := p.Generate(context.Background(), &PromptRequest{}, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assertResponse(t, resp)
+	})
 }
 
-func TestExecuteWithModelName(t *testing.T) {
-	ai.DefineModel("test", "test", nil, testGenerate)
-	p, err := New("TestExecute", "TestExecute", Config{ModelName: "test/test"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := p.Generate(context.Background(), &PromptRequest{}, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+func assertResponse(t *testing.T, resp *ai.GenerateResponse) {
 	if len(resp.Candidates) != 1 {
 		t.Errorf("got %d candidates, want 1", len(resp.Candidates))
 		if len(resp.Candidates) < 1 {
