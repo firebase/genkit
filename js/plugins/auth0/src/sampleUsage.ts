4dea3ff7ff0e1903dd100cff6cb31e8d450d4a65
Jeff Huang <huangjeff@google.com>
2024-10-03 14:59:57 -0500

    example

--- js/plugins/auth0/src/sampleUsage.ts
+++ js/plugins/auth0/src/sampleUsage.ts
@@ -1,4 +1,6 @@
-/*
+
+
+// Example usage to trigger the flow
 await runFlow(
   yourAuthFlow,
   "your input string",
@@ -6,4 +8,6 @@ await runFlow(
     withLocalAuthContext: "your-auth0-access-token"
   }
 );
-*/
+
+// You will wrap this around an HTTP endpoint of your choice (e.g. express endpoint)
+// And then parse the eader to grab the Authorization token and pass that in to this method
