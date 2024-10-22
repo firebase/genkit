93fda188f5893fafeb9d4234f722194e8d39ac85
Pavel Jbanov <pavelj@google.com>
2024-10-22 14:00:46 -0400

    chore: added release scripts (#1098)

---
+++ scripts/release_main.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# git clone git@github.com:firebase/genkit.git
+# cd genkit
+# pnpm i
+# pnpm build
+# pnpm test:all
+
+# Run from root: scripts/release_main.sh
+
+pnpm login --registry https://wombat-dressing-room.appspot.com
+
+
+CURRENT=`pwd`
+
+cd genkit-tools/common
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd genkit-tools/cli
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/core
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/ai
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/flow
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/dotprompt
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/chroma
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/dev-local-vectorstore
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/firebase
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/google-cloud
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/googleai
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/ollama
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/pinecone
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd js/plugins/vertexai
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd  js/plugins/evaluators
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
+cd  js/plugins/langchain
+pnpm publish --registry https://wombat-dressing-room.appspot.com
+cd $CURRENT
+
