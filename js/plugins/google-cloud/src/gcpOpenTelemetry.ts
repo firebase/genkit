5be16f063bbc42c14b04725a00ecc1cc644956f4
Max Lord <maxlord@google.com>
2024-11-20 13:03:53 -0500

    Marking failed paths in google-cloud plugin telemetry (#1353)

--- js/plugins/google-cloud/src/gcpOpenTelemetry.ts
+++ js/plugins/google-cloud/src/gcpOpenTelemetry.ts
@@ -312,7 +312,7 @@ class AdjustingTraceExporter implements SpanExporter {
 
       span = this.redactInputOutput(span);
       span = this.markErrorSpanAsError(span);
-      span = this.markFailedAction(span);
+      span = this.markFailedSpan(span);
       span = this.markGenkitFeature(span);
       span = this.markGenkitModel(span);
       span = this.normalizeLabels(span);
@@ -401,20 +401,25 @@ class AdjustingTraceExporter implements SpanExporter {
     };
   }
 
-  private markFailedAction(span: ReadableSpan): ReadableSpan {
+  private markFailedSpan(span: ReadableSpan): ReadableSpan {
     if (
       span.attributes['genkit:state'] === 'error' &&
       (span.attributes['genkit:type'] === 'action' ||
-        span.attributes['genkit:type'] === 'flowStep') &&
-      span.attributes['genkit:name']
+        span.attributes['genkit:type'] === 'flowStep' ||
+        span.attributes['genkit:type'] === 'helper')
     ) {
-      span.attributes['genkit:failedSpan'] = span.attributes['genkit:name'];
+      if (!!span.attributes['genkit:name']) {
+        span.attributes['genkit:failedSpan'] = span.attributes['genkit:name'];
+      }
+      if (!!span.attributes['genkit:path']) {
+        span.attributes['genkit:failedPath'] = span.attributes['genkit:path'];
+      }
     }
     return span;
   }
 
   private markGenkitFeature(span: ReadableSpan): ReadableSpan {
-    if (span.attributes['genkit:isRoot'] && span.attributes['genkit:name']) {
+    if (span.attributes['genkit:isRoot'] && !!span.attributes['genkit:name']) {
       span.attributes['genkit:feature'] = span.attributes['genkit:name'];
     }
     return span;
@@ -423,7 +428,7 @@ class AdjustingTraceExporter implements SpanExporter {
   private markGenkitModel(span: ReadableSpan): ReadableSpan {
     if (
       span.attributes['genkit:metadata:subtype'] === 'model' &&
-      span.attributes['genkit:name']
+      !!span.attributes['genkit:name']
     ) {
       span.attributes['genkit:model'] = span.attributes['genkit:name'];
     }
