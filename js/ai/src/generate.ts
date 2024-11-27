c36cad74555c69f8537af48b94b8dfbc11b77c24
Michael Bleigh <bleigh@google.com>
2024-11-27 11:13:51 -0800

    fix(js/ai): Fixes use of namespaced tools in model calls. (#1423)

--- js/ai/src/generate.ts
+++ js/ai/src/generate.ts
@@ -129,7 +129,7 @@ export async function toGenerateRequest(
     messages: injectInstructions(messages, instructions),
     config: options.config,
     docs: options.docs,
-    tools: tools?.map((tool) => toToolDefinition(tool)) || [],
+    tools: tools?.map(toToolDefinition) || [],
     output: {
       ...(resolvedFormat?.config || {}),
       schema: resolvedSchema,
