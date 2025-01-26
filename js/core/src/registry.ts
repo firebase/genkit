cc60404efaf01d5736925f312e380f5fb711e65f
Pavel Jbanov <pavelj@google.com>
2025-01-26 10:28:18 -0500

    fix

--- js/core/src/registry.ts
+++ js/core/src/registry.ts
@@ -165,9 +165,11 @@ export class Registry {
   async listActions(): Promise<ActionsRecord> {
     await this.initializeAllPlugins();
     const actions: Record<string, Action<z.ZodTypeAny, z.ZodTypeAny>> = {};
-    await Promise.all(Object.entries(this.actionsById).map(async ([key,action]) => {
-      actions[key] = await action;
-    });
+    await Promise.all(
+      Object.entries(this.actionsById).map(async ([key, action]) => {
+        actions[key] = await action;
+      })
+    );
     return {
       ...(await this.parent?.listActions()),
       ...actions,
