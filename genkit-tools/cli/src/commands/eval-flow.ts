aff711981ef92b189d25a5e134b783e3643d01c0
ssbushi <66321939+ssbushi@users.noreply.github.com>
2025-01-27 10:39:50 -0500

    feat: more specific errors for evals (#1626)

--- genkit-tools/cli/src/commands/eval-flow.ts
+++ genkit-tools/cli/src/commands/eval-flow.ts
@@ -31,6 +31,7 @@ import {
 } from '@genkit-ai/tools-common/eval';
 import {
   confirmLlmUse,
+  hasAction,
   loadInferenceDatasetFile,
   logger,
 } from '@genkit-ai/tools-common/utils';
@@ -87,10 +88,16 @@ export const evalFlow = new Command('eval:flow')
   .action(
     async (flowName: string, data: string, options: EvalFlowRunCliOptions) => {
       await runWithManager(async (manager) => {
+        const actionRef = `/flow/${flowName}`;
         if (!data && !options.input) {
           throw new Error(
             'No input data passed. Specify input data using [data] argument or --input <filename> option'
           );
+        }
+
+        const hasTargetAction = await hasAction({ manager, actionRef });
+        if (!hasTargetAction) {
+          throw new Error(`Cannot find action ${actionRef}.`);
         }
 
         let evaluatorActions: Action[];
@@ -103,6 +110,13 @@ export const evalFlow = new Command('eval:flow')
           evaluatorActions = await getMatchingEvaluatorActions(
             manager,
             evalActionKeys
+          );
+        }
+        if (!evaluatorActions.length) {
+          throw new Error(
+            options.evaluators
+              ? `No matching evaluators found for '${options.evaluators}'`
+              : `No evaluators found in your app`
           );
         }
         logger.debug(
@@ -126,7 +140,6 @@ export const evalFlow = new Command('eval:flow')
           );
         }
 
-        const actionRef = `/flow/${flowName}`;
         const inferenceDataset = await readInputs(
           sourceType,
           data,
--- genkit-tools/common/src/utils/eval.ts
+++ genkit-tools/common/src/utils/eval.ts
@@ -19,6 +19,7 @@ import { createReadStream } from 'fs';
 import { readFile } from 'fs/promises';
 import * as inquirer from 'inquirer';
 import { createInterface } from 'readline';
+import { RuntimeManager } from '../manager';
 import {
   EvalField,
   EvaluationExtractor,
@@ -318,4 +319,14 @@ async function readLines(fileName: string): Promise<string[]> {
     lines.push(line);
   }
   return lines;
+}
+
+export async function hasAction(params: {
+  manager: RuntimeManager;
+  actionRef: string;
+}): Promise<boolean> {
+  const { manager, actionRef } = { ...params };
+  const actionsRecord = await manager.listActions();
+
+  return actionsRecord.hasOwnProperty(actionRef);
 }
