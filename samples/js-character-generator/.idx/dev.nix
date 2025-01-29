af71fd7cb0227366ebce289a16df2501df9a6a8e
Sam Phillips <samphillips@google.com>
2025-01-29 12:33:58 -0500

    feat: Move IDX template from idx github to Genkit github (#1681)
    * feat: Move IDX template from idx github to Genkit github to make it easier to maintain

---
+++ samples/js-character-generator/.idx/dev.nix
@@ -0,0 +1,38 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.util-linux
+    # pkgs.go
+  ];
+  # Sets environment variables in the workspace
+  env = {
+    #TODO Get a API key from https://g.co/ai/idxGetGeminiKey 
+    GOOGLE_GENAI_API_KEY = "TODO"; 
+  };
+  idx = {
+    internal.templates-cli.enable = true;
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      # "golang.go"
+    ];
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        default.openFiles = [ "README.md" "index.ts" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        run-server = "npm run dev";
+      };
+    };
+  };
+}
