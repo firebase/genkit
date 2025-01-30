cdfd77fb527a918fa710b69dcfc66ad2473abdf7
Sam Phillips <samphillips@google.com>
2025-01-30 11:35:59 -0500

    Removes package lock from sample

--- samples/js-character-generator/idx-template.nix
+++ samples/js-character-generator/idx-template.nix
@@ -7,7 +7,6 @@
     mkdir "$out"/.idx
     cp -r ${./.idx}/. "$out/.idx/"
     cp -f ${./package.json} "$out/package.json"
-    cp -f ${./package-lock.json} "$out/package-lock.json"
     cp -f ${./index.ts} "$out/index.ts"
     cp -f ${./.gitignore} "$out/.gitignore"
     cp ${./README_IDX.md} "$out"/README.md
