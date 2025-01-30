24493164dd20509d3e82ecedfe8ba8a2b1c45996
Thomas Bouldin <inlined@users.noreply.github.com>
2025-01-29 17:08:06 -0800

    feat(js): Add @genkit-ai/next plugin (#1676)
    Does not yet support auth policies et. al. Will propose a big refactor for all of that stuff.

---
+++ js/testapps/next/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Genkit Next.js sample app',
+  description: 'Test app for the @genkit-ai/next plugin',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
