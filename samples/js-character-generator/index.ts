11a3a827918ebdf6fafd53eff38b84a3123406a3
Sam Phillips <samphillips@google.com>
2025-01-30 21:21:12 -0500

    fix: prettier fixes

--- samples/js-character-generator/index.ts
+++ samples/js-character-generator/index.ts
@@ -21,29 +21,27 @@ const ai = genkit({
   plugins: [googleAI()],
 });
 
-const prompt = ai.definePrompt(
-  {
-    name: 'Character Prompt',
-    model: gemini15Flash,
-    input: {
-      schema: z.object({
-        inspiration: z.string(),
-      }),
-    },
-    output: {
-      format: 'json',
-      schema: z.object({
-        name: z.string(),
-        strength: z.number(),
-        intelligence: z.number(),
-        description: z.string(),
-      }),
-    },
+const prompt = ai.definePrompt({
+  name: 'Character Prompt',
+  model: gemini15Flash,
+  input: {
+    schema: z.object({
+      inspiration: z.string(),
+    }),
+  },
+  output: {
+    format: 'json',
+    schema: z.object({
+      name: z.string(),
+      strength: z.number(),
+      intelligence: z.number(),
+      description: z.string(),
+    }),
+  },
   prompt: `You're a expert DnD designer, create a new character.
     Base the character on {{inspiration}} but don't make it
-    and exact match.`
-  }
-);
+    an exact match.`,
+});
 
 (async () => {
   console.log((await prompt({ inspiration: 'Yogi Berra' })).output);
