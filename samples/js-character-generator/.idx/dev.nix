96df851389eb85fca3871b6b5c041f5d190f05d5
Sam Phillips <samphillips@google.com>
2025-02-02 20:31:18 -0500

    fix: fix idx-template.json

--- samples/js-character-generator/.idx/dev.nix
+++ samples/js-character-generator/.idx/dev.nix
@@ -15,7 +15,6 @@
     GOOGLE_GENAI_API_KEY = "TODO"; 
   };
   idx = {
-    internal.templates-cli.enable = true;
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
