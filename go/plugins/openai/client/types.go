ed58569c7c06c4ab0ad52f298bcd22d144f22043
kekoawong <kekoawong10@gmail.com>
2025-02-23 19:17:52 -0500

    Updated comments

--- go/plugins/openai/client/types.go
+++ go/plugins/openai/client/types.go
@@ -13,12 +13,12 @@ type ChatRequest struct {
 	Messages []ChatMessage `json:"messages"`
 
 	// Optional
-	Temperature         float64  `json:"temperature,omitempty"`           // Default: 1. Range: 0-2
-	TopP                float64  `json:"top_p,omitempty"`                 // Default: 1. Range: 0-1
-	N                   int      `json:"n,omitempty"`                     // Default: 1
-	Stream              bool     `json:"stream,omitempty"`                // Default: false
-	Stop                []string `json:"stop,omitempty"`                  // Default: null
-	MaxCompletionTokens int      `json:"max_completion_tokens,omitempty"` // Default: inf
+	Temperature         float64  `json:"temperature,omitempty"`
+	TopP                float64  `json:"top_p,omitempty"`
+	N                   int      `json:"n,omitempty"`
+	Stream              bool     `json:"stream,omitempty"`
+	Stop                []string `json:"stop,omitempty"`
+	MaxCompletionTokens int      `json:"max_completion_tokens,omitempty"`
 }
 
 // Role defines the possible roles in a chat conversation
