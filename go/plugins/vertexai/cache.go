e9d2305c354ad00637ccc3e86b1f623a0be965b4
Hugo Aguirre Parra <aguirreparra@google.com>
2025-02-24 22:11:40 +0000

    docs: update licences

--- go/plugins/vertexai/cache.go
+++ go/plugins/vertexai/cache.go
@@ -1,16 +1,5 @@
 // Copyright 2025 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 package vertexai
 
@@ -38,15 +27,13 @@ type CacheConfigDetails struct {
 	TTL time.Duration
 }
 
-var (
-	INVALID_ARGUMENT_MESSAGES = struct {
-		modelVersion string
-		tools        string
-	}{
-		modelVersion: "Invalid modelVersion specified.",
-		tools:        "Tools are not supported with context caching.",
-	}
-)
+var INVALID_ARGUMENT_MESSAGES = struct {
+	modelVersion string
+	tools        string
+}{
+	modelVersion: "Invalid modelVersion specified.",
+	tools:        "Tools are not supported with context caching.",
+}
 
 // getContentForCache inspects the request and modelVersion, and constructs a
 // genai.CachedContent that should be cached.
@@ -253,7 +240,6 @@ func handleCacheIfNeeded(
 	modelVersion string,
 	cacheConfig *CacheConfigDetails,
 ) (*genai.CachedContent, error) {
-
 	if cacheConfig == nil || validateContextCacheRequest(request, modelVersion) != nil {
 		return nil, nil
 	}
