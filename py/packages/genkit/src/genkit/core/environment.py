47fd4ad1e8a6ba00c25fb825ac2d83ed45e50eb5
Yesudeep Mangalapilly <yesudeep@google.com>
2025-02-23 22:39:52 -0800

    feat(py): Python implementation for utils.ts, error.ts, and statusTypes.ts
    ISSUES:
    - [ ] https://github.com/firebase/genkit/issues/2073 (error.ts)
    - [ ] https://github.com/firebase/genkit/issues/2074 (statusTypes.ts)
    - [ ] https://github.com/firebase/genkit/issues/2075 (utils.ts)
    
    CHANGELOG:
    - [ ] utils.ts -> environment.py
    - [ ] error.ts -> error.py
    - [ ] statusTypes -> status_types.py

--- py/packages/genkit/src/genkit/core/environment.py
+++ py/packages/genkit/src/genkit/core/environment.py
@@ -26,4 +26,28 @@ def is_dev_environment() -> bool:
     Returns:
         True if the current environment is a development environment.
     """
-    return os.getenv(EnvVar.GENKIT_ENV) == GenkitEnvironment.DEV
+    return get_current_environment() == GenkitEnvironment.DEV
+
+
+def is_prod_environment() -> bool:
+    """Returns True if the current environment is a production environment.
+
+    Returns:
+        True if the current environment is a production environment.
+    """
+    return get_current_environment() == GenkitEnvironment.PROD
+
+
+def get_current_environment() -> GenkitEnvironment:
+    """Returns the current environment.
+
+    Returns:
+        The current environment.
+    """
+    env = os.getenv(EnvVar.GENKIT_ENV)
+    if env is None:
+        return GenkitEnvironment.PROD
+    try:
+        return GenkitEnvironment(env)
+    except ValueError:
+        return GenkitEnvironment.PROD
