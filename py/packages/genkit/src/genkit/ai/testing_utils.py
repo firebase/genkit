6441d22fe45bc5c915ca010c33a7a7207d5735d2
Pavel Jbanov <pavelj@google.com>
2025-02-27 09:07:22 -0500

    feedback

--- py/packages/genkit/src/genkit/ai/testing_utils.py
+++ py/packages/genkit/src/genkit/ai/testing_utils.py
@@ -28,7 +28,7 @@ class ProgrammableModel:
     def model_fn(self, request: GenerateRequest, ctx: ActionRunContext):
         self.last_request = request
         response = self.responses[self.request_idx]
-        if self.chunks is not None:
+        if self.chunks:
             for chunk in self.chunks[self.request_idx]:
                 ctx.send_chunk(chunk)
         self.request_idx += 1
@@ -52,34 +52,33 @@ class EchoModel:
     def __init__(self):
         self.last_request: GenerateRequest = None
 
-        def model_fn(request: GenerateRequest):
-            self.last_request = request
-            merged_txt = ''
-            for m in request.messages:
-                merged_txt += f' {m.role}: ' + ','.join(
-                    dump_json(p.root.text) if p.root.text is not None else '""'
-                    for p in m.content
-                )
-            echo_resp = f'[ECHO]{merged_txt}'
-            if request.config:
-                echo_resp += f' {dump_json(request.config)}'
-            if request.tool_choice is not None:
-                echo_resp += f' tool_choice={request.tool_choice}'
-            if request.output and dump_json(request.output) != '{}':
-                echo_resp += f' output={dump_json(request.output)}'
-            return GenerateResponse(
-                message=Message(
-                    role=Role.MODEL, content=[TextPart(text=echo_resp)]
-                )
+    def model_fn(self, request: GenerateRequest):
+        self.last_request = request
+        merged_txt = ''
+        for m in request.messages:
+            merged_txt += f' {m.role}: ' + ','.join(
+                dump_json(p.root.text) if p.root.text is not None else '""'
+                for p in m.content
             )
-
-        self.model_fn = model_fn
+        echo_resp = f'[ECHO]{merged_txt}'
+        if request.config:
+            echo_resp += f' {dump_json(request.config)}'
+        if request.tool_choice is not None:
+            echo_resp += f' tool_choice={request.tool_choice}'
+        if request.output and dump_json(request.output) != '{}':
+            echo_resp += f' output={dump_json(request.output)}'
+        return GenerateResponse(
+            message=Message(role=Role.MODEL, content=[TextPart(text=echo_resp)])
+        )
 
 
 def define_echo_model(ai: Genkit, name: str = 'echoModel'):
     """Defines a simple echo model that echos requests"""
     echo = EchoModel()
 
-    action = ai.define_model(name=name, fn=echo.model_fn)
+    def model_fn(request: GenerateRequest):
+        return echo.model_fn(request)
+
+    action = ai.define_model(name=name, fn=model_fn)
 
     return (echo, action)
