695c46fa412527ba846bdc4038d60dc551c3c11d
Yesudeep Mangalapilly <yesudeep@google.com>
2025-02-27 19:28:31 -0800

    fix(go): bin/fmt to format go code #2193 (#2194)
    ISSUE: https://github.com/firebase/genkit/issues/2193
    
    See: https://goreportcard.com/report/github.com/firebase/genkit/go

--- go/internal/doc-snippets/doc.go
+++ go/internal/doc-snippets/doc.go
@@ -1,7 +1,6 @@
 // Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-
 // Package snippets contains snippets that are incorporated into
 // documentation (see the docs-go directory under the repo root).
 package snippets
