5aa0fa34cadf26d5e31630f224c3ab513924da8f
Hugo Aguirre Parra <aguirreparra@google.com>
2025-03-04 21:02:43 +0000

    fix: add vertexai changes

--- go/plugins/vertexai/cache.go
+++ go/plugins/vertexai/cache.go
@@ -5,39 +5,39 @@ package vertexai
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"slices"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/vertexai/genai"
 	"github.com/firebase/genkit/go/ai"
+	"google.golang.org/api/iterator"
 )
 
-// DEFAULT_TTL in seconds (5 minutes)
-const DEFAULT_TTL = 300
-
-// CacheConfigDetails holds configuration details for caching.
-// Adjust fields as needed for your use case.
-type CacheConfigDetails struct {
-	// TTL in seconds is how long to keep the cached content.
-	// If zero, defaults to 60 minutes.
-	TTL time.Duration
-}
-
 var ContextCacheSupportedModels = [...]string{
+	"gemini-2.0-flash-lite-001",
+	"gemini-2.0-flash-001",
+
 	"gemini-1.5-flash-001",
+	"gemini-1.5-flash-002",
+
 	"gemini-1.5-pro-001",
+	"gemini-1.5-pro-002",
 }
 
 var INVALID_ARGUMENT_MESSAGES = struct {
 	modelVersion string
 	tools        string
+	systemPrompt string
 }{
 	modelVersion: fmt.Sprintf(
 		"unsupported model version, expected: %s",
 		strings.Join(ContextCacheSupportedModels[:], ", ")),
-	tools: "tools are not supported with context caching",
-	// TODO: add system prompt constraint to avoid grpc error
+	tools:        "tools are not supported with context caching",
+	systemPrompt: "system prompts are not supported with context caching",
 }
 
 // getContentForCache inspects the request and modelVersion, and constructs a
@@ -46,27 +46,22 @@ var INVALID_ARGUMENT_MESSAGES = struct {
 func getContentForCache(
 	request *ai.ModelRequest,
 	modelVersion string,
-	cacheConfig *CacheConfigDetails,
 ) (*genai.CachedContent, error) {
-	endOfCachedContents, extractedCacheConfig, err := extractCacheConfig(request)
+	cacheEndIdx, ttl, err := findCacheMarker(request)
 	if err != nil {
 		return nil, err
 	}
+	// no cache message found
+	if cacheEndIdx == -1 {
+		return nil, err
+	}
 
-	// If no cache metadata found, return nil
-	if extractedCacheConfig == nil {
-		return nil, nil
-	}
-	if endOfCachedContents < 0 || endOfCachedContents >= len(request.Messages) {
-		return nil, fmt.Errorf("invalid endOfCachedContents index")
-	}
-	if cacheConfig == nil {
-		cacheConfig = extractedCacheConfig
+	if cacheEndIdx < 0 || cacheEndIdx >= len(request.Messages) {
+		return nil, fmt.Errorf("end of cached contents, index %d is invalid", cacheEndIdx)
 	}
 
 	var messagesForCache []*genai.Content
-	for i := endOfCachedContents; i >= 0; i-- {
-		// system instructions should not be used for cache
+	for i := cacheEndIdx; i >= 0; i-- {
 		if request.Messages[i].Role == ai.RoleSystem {
 			continue
 		}
@@ -83,78 +78,98 @@ func getContentForCache(
 	return &genai.CachedContent{
 		Model:      modelVersion,
 		Contents:   messagesForCache,
-		Expiration: genai.ExpireTimeOrTTL{TTL: calculateTTL(cacheConfig.TTL)},
+		Expiration: genai.ExpireTimeOrTTL{TTL: ttl},
 	}, nil
-}
-
-// calculateTTL returns the TTL as a time.Duration.
-func calculateTTL(ttl time.Duration) time.Duration {
-	if ttl <= 0 {
-		return DEFAULT_TTL
-	}
-	return ttl * time.Second
-}
-
-// contains checks if a slice contains a given string.
-func contains(slice []string, target string) bool {
-	for _, s := range slice {
-		if s == target {
-			return true
-		}
-	}
-	return false
 }
 
 // validateContextCacheRequest checks for supported models and checks if Tools
 // are being provided in the request
 func validateContextCacheRequest(request *ai.ModelRequest, modelVersion string) error {
-	if modelVersion == "" || !contains(ContextCacheSupportedModels[:], modelVersion) {
+	if modelVersion == "" || !slices.Contains(ContextCacheSupportedModels[:], modelVersion) {
 		return fmt.Errorf("%s", INVALID_ARGUMENT_MESSAGES.modelVersion)
 	}
 	if len(request.Tools) > 0 {
 		return fmt.Errorf("%s", INVALID_ARGUMENT_MESSAGES.tools)
 	}
+	for _, m := range request.Messages {
+		if m.Role == ai.RoleSystem {
+			return fmt.Errorf("%s", INVALID_ARGUMENT_MESSAGES.systemPrompt)
+		}
+	}
 
-	// If we reach here, request is valid for context caching
 	return nil
 }
 
-func extractCacheConfig(request *ai.ModelRequest) (int, *CacheConfigDetails, error) {
-	endOfCachedContents := -1
-	var cacheConfig *CacheConfigDetails
-
-	// find the cache mark in the list of request messages
+// findCacheMarker finds the cache mark in the list of request messages.
+// All of the messages preceding this mark will be cached
+func findCacheMarker(request *ai.ModelRequest) (cacheEndIdx int, ttl time.Duration, err error) {
 	for i := len(request.Messages) - 1; i >= 0; i-- {
 		m := request.Messages[i]
 		if m.Metadata == nil {
 			continue
 		}
 
-		// found the cache key and its content is a map
-		if c, ok := m.Metadata["cache"].(map[string]any); ok && c != nil {
-			// Found a message with `metadata.cache`
-			endOfCachedContents = i
+		cacheVal, exists := m.Metadata["cache"]
+		if !exists || cacheVal == nil {
+			continue
+		}
 
-			// only accepting ints for ttlSeconds
-			if ttlVal, ok := c["ttlSeconds"].(int); ok {
-				cacheConfig = &CacheConfigDetails{
-					TTL: time.Duration(ttlVal),
-				}
-			} else {
-				return -1, nil, fmt.Errorf("invalid type for cache ttlSeconds, expected int, got %T", ttlVal)
-			}
+		c, ok := cacheVal.(map[string]any)
+		if !ok {
+			return -1, 0, fmt.Errorf("cache metadata should be map but got: %T", cacheVal)
+		}
+
+		if t, ok := c["ttlSeconds"].(int); ok {
+			fmt.Printf("found ttlSeconds: %d\n\n", t)
+			return i, time.Duration(t) * time.Second, nil
+		}
+
+		return -1, 0, fmt.Errorf("invalid type for cache ttlSeconds, expected int, got %T", c["ttlSeconds"])
+	}
+	return -1, 0, nil
+}
+
+// lookupCache retrieves a *genai.CachedContent from a given cache key
+func lookupCache(ctx context.Context, client *genai.Client, key string) (*genai.CachedContent, error) {
+	if key == "" {
+		return nil, fmt.Errorf("empty cache key detected")
+	}
+
+	it := client.ListCachedContents(ctx)
+	for {
+		cc, err := it.Next()
+		if err == iterator.Done {
 			break
-		} else {
-			return -1, nil, fmt.Errorf("cache metadata should be a map but got %T", m.Metadata["cache"])
+		}
+		if err != nil {
+			return nil, err
+		}
+		if key == cc.DisplayName {
+			return cc, nil
 		}
 	}
 
-	if endOfCachedContents == -1 {
-		// No cache metadata found
-		return -1, nil, nil
-	}
+	return nil, nil
+}
 
-	return endOfCachedContents, cacheConfig, nil
+// genCacheKey generates a sha256 key for cached content used to
+// create or lookup caches during generate requests
+func genCacheKey(content *genai.CachedContent) string {
+	hash := sha256.New()
+
+	// Incorporate content parts to ensure uniqueness
+	for _, c := range content.Contents {
+		for _, p := range c.Parts {
+			switch v := p.(type) {
+			case genai.Text:
+				hash.Write([]byte(v))
+			case genai.Blob:
+				hash.Write([]byte(v.MIMEType))
+				hash.Write(v.Data)
+			}
+		}
+	}
+	return hex.EncodeToString(hash.Sum(nil))
 }
 
 // handleCacheIfNeeded checks if caching should be used, attempts to find or create the cache,
@@ -164,12 +179,7 @@ func handleCacheIfNeeded(
 	client *genai.Client,
 	request *ai.ModelRequest,
 	modelVersion string,
-	cacheConfig *CacheConfigDetails,
 ) (*genai.CachedContent, error) {
-	if cacheConfig == nil {
-		return nil, nil
-	}
-
 	// since context caching is only available for specific model versions, we
 	// must make sure the configuration has the right version
 	if c, ok := request.Config.(*ai.GenerationCommonConfig); ok {
@@ -181,11 +191,23 @@ func handleCacheIfNeeded(
 	}
 
 	// obtain message parts to be cached
-	cc, err := getContentForCache(request, modelVersion, cacheConfig)
+	cc, err := getContentForCache(request, modelVersion)
 	if err != nil {
 		return nil, err
 	}
 
-	// cc.Model = modelVersion
-	return client.CreateCachedContent(ctx, cc)
+	// generate cache key and check if cache already exists before creating one
+	key := genCacheKey(cc)
+	newCache, err := lookupCache(ctx, client, key)
+	if err != nil {
+		return nil, err
+	}
+	if newCache != nil {
+		return client.GetCachedContent(ctx, newCache.Name)
+	}
+
+	cc.DisplayName = key
+	newCache, err = client.CreateCachedContent(ctx, cc)
+
+	return newCache, nil
 }
--- go/plugins/vertexai/cache_test.go
+++ go/plugins/vertexai/cache_test.go
@@ -1,7 +1,7 @@
 // Copyright 2025 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-package googleai
+package vertexai
 
 import (
 	"context"
