c12cc815891eff33b864b1abefdc0112c0334e77
Pavel Jbanov <pavelj@google.com>
2025-03-18 15:49:56 -0400

    fix({go,cli}): relax time event shema

--- genkit-tools/telemetry-server/src/index.ts
+++ genkit-tools/telemetry-server/src/index.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { TraceData } from '@genkit-ai/tools-common';
+import { TraceDataSchema } from '@genkit-ai/tools-common';
 import express from 'express';
 import * as http from 'http';
 import { TraceStore } from './types';
@@ -48,27 +48,40 @@ export function startTelemetryServer(params: {
     response.status(200).send('OK');
   });
 
-  api.get('/api/traces/:traceId', async (request, response) => {
-    const { traceId } = request.params;
-    response.json(await params.traceStore.load(traceId));
+  api.get('/api/traces/:traceId', async (request, response, next) => {
+    try {
+      const { traceId } = request.params;
+      response.json(await params.traceStore.load(traceId));
+    } catch (e) {
+      next(e);
+    }
   });
 
-  api.post('/api/traces', async (request, response) => {
-    const traceData = request.body as TraceData;
-    await params.traceStore.save(traceData.traceId, traceData);
-    response.status(200).send('OK');
+  api.post('/api/traces', async (request, response, next) => {
+    console.log('post trace:\n' + JSON.stringify(request.body, undefined, 2));
+    try {
+      const traceData = TraceDataSchema.parse(request.body);
+      await params.traceStore.save(traceData.traceId, traceData);
+      response.status(200).send('OK');
+    } catch (e) {
+      next(e);
+    }
   });
 
-  api.get('/api/traces', async (request, response) => {
-    const { limit, continuationToken } = request.query;
-    response.json(
-      await params.traceStore.list({
-        limit: limit ? parseInt(limit.toString()) : undefined,
-        continuationToken: continuationToken
-          ? continuationToken.toString()
-          : undefined,
-      })
-    );
+  api.get('/api/traces', async (request, response, next) => {
+    try {
+      const { limit, continuationToken } = request.query;
+      response.json(
+        await params.traceStore.list({
+          limit: limit ? parseInt(limit.toString()) : undefined,
+          continuationToken: continuationToken
+            ? continuationToken.toString()
+            : undefined,
+        })
+      );
+    } catch (e) {
+      next(e);
+    }
   });
 
   api.use((err: any, req: any, res: any, next: any) => {
