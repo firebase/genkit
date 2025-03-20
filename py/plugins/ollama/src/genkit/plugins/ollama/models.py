b95767a24494b0b6e803a53a556dfcbac790302d
Pavel Jbanov <pavelj@google.com>
2025-03-20 09:20:54 -0400

    py fmt

--- py/plugins/ollama/src/genkit/plugins/ollama/models.py
+++ py/plugins/ollama/src/genkit/plugins/ollama/models.py
@@ -4,9 +4,9 @@ import logging
 import mimetypes
 from typing import Literal
 
-import ollama as ollama_api
 from pydantic import BaseModel, Field, HttpUrl
 
+import ollama as ollama_api
 from genkit.ai.model import get_basic_usage_stats
 from genkit.core.action import ActionRunContext
 from genkit.core.typing import (
