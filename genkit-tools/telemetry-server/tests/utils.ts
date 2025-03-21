dabd6d3bad15579136275a76116c53adbc5178ca
Pavel Jbanov <pavelj@google.com>
2025-03-21 12:14:36 -0400

    refactor(cli): implemented basic index for the telemetry server which allows filtering

---
+++ genkit-tools/telemetry-server/tests/utils.ts
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { SpanData } from '@genkit-ai/tools-common';
+
+export function span(
+  traceId: string,
+  id: string,
+  inputLength: number | undefined,
+  outputLength: number | undefined
+): SpanData {
+  const attributes = {
+    'genkit:type': 'flow',
+  };
+  if (inputLength) {
+    attributes['genkit:input'] = generateString(inputLength);
+  }
+  if (outputLength) {
+    attributes['genkit:output'] = generateString(outputLength);
+  }
+  return {
+    traceId: traceId,
+    spanId: id,
+    displayName: `Span ${id}`,
+    startTime: 1,
+    endTime: 2,
+    instrumentationLibrary: { name: 'genkit' },
+    spanKind: 'INTERNAL',
+    attributes,
+  } as SpanData;
+}
+
+function generateString(length: number) {
+  let str = '';
+  while (str.length < length) {
+    str += 'blah ';
+  }
+  return str.substring(0, length);
+}
+
+export async function sleep(ms) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
