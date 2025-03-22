8237d1664123c035c08f38b5c797f16122860642
Yesudeep Mangalapilly <yesudeep@google.com>
2025-03-21 18:00:07 -0700

    fix: full license header with SPDX license identifier #2176
    ISSUE: #2176
    
    CHANGELOG:
    - [ ] Update the `bin/add_license` and `bin/check_license` scripts
          to include and check the full license header in addition
          to the SPDX license identifier
    - [ ] Update license headers for Go and Python source code.
    - [ ] Update `jsonschemagen.go` to also add the correct license header.
    - [ ] Update json schema golden
    - [ ] Test across go versions 1.22 through 1.24

--- py/plugins/vertex-ai/tests/test_imagen.py
+++ py/plugins/vertex-ai/tests/test_imagen.py
@@ -1,9 +1,6 @@
 # Copyright 2025 Google LLC
 # SPDX-License-Identifier: Apache-2.0
 
-# Copyright 2025 Google LLC
-# SPDX-License-Identifier: Apache-2.0
-
 """Test Gemini models."""
 
 import pytest
