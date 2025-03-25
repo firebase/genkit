0b417232e649a602186d00c139e096f97beda978
Hugo Aguirre Parra <aguirreparra@google.com>
2025-03-25 17:07:24 +0000

    fix: main merge errors

--- go/samples/prompts-dir/main.go
+++ go/samples/prompts-dir/main.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
 // [START main]
@@ -12,20 +12,19 @@ import (
 	// Import Genkit and the Google AI plugin
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
-	"github.com/firebase/genkit/go/plugins/googleai"
+	"github.com/firebase/genkit/go/plugins/googlegenai"
 )
 
 func main() {
 	ctx := context.Background()
 
-	g, err := genkit.Init(ctx, genkit.WithPlugins(&googleai.GoogleAI{}))
+	g, err := genkit.Init(ctx, genkit.WithPlugins(&googlegenai.GoogleAI{}))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Define a simple flow that prompts an LLM to generate menu suggestions.
 	genkit.DefineFlow(g, "menuSuggestionFlow", func(ctx context.Context, input any) (string, error) {
-
 		// Look up the prompt by name
 		prompt := genkit.LookupPrompt(g, "local", "example")
 		if prompt == nil {
