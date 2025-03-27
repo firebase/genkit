b6ec90452260b1175fea3e2261b51e1429686427
Pavel Jbanov <pavelj@google.com>
2025-03-27 08:36:34 -0400

    make reflection api thread a daemon

--- py/packages/genkit-ai/src/genkit/ai/veneer.py
+++ py/packages/genkit-ai/src/genkit/ai/veneer.py
@@ -163,6 +163,7 @@ class Genkit(GenkitRegistry):
             self.thread = threading.Thread(
                 target=self.start_server,
                 args=[reflection_server_spec, self.loop],
+                daemon=True,
             )
             self.thread.start()
         else:
