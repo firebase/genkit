beafe179933c3af19e426b417db78769d78c9d8f
Michael Bleigh <bleigh@google.com>
2025-03-31 10:33:00 -0700

    format

--- js/plugins/firebase/src/beta/data-connect.ts
+++ js/plugins/firebase/src/beta/data-connect.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
   FirebaseApp,
   FirebaseOptions,
@@ -40,9 +56,10 @@ export interface DataConnectToolsOptions {
   configFile: string;
   /**
    * Your Firebase client SDK config or a FirebaseApp or a FirebaseServerApp. If not provided
-   * the plugin will attempt to use the default app.
+   * the plugin will attempt to use `context.firebaseApp` for an in-context app or will fall
+   * back the default app.
    */
-  sdk?: FirebaseOptions | FirebaseApp;
+  firebaseApp?: FirebaseOptions | FirebaseApp;
   /**
    * How to handle errors coming from Data Connect tools:
    *
