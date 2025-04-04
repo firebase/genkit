45ff4308fbd8231d080a39b0b6f2fd52a0dcc392
Yesudeep Mangalapilly <yesudeep@google.com>
2025-04-04 07:53:10 -0700

    fix(py): try tox; also use nox-specified python with pytest in noxfile; fix more 3.10 compatibility issues
    | Test Runner | Parallel Tests | Isolated Environments | UV support |
    |-------------|----------------|-----------------------|------------|
    | tox         | [x]            | [x]                   | [x]        |
    | nox         | [ ]            | [x]                   | [x]        |
    
    ```
    zsh❯ py/bin/run_python_tests_with_tox
    Running Python tests via tox...
    py310: OK ✔ in 7.02 seconds
    py311: OK ✔ in 7.3 seconds
    py313: OK ✔ in 7.48 seconds
      py310: OK (7.02=setup[0.01]+cmd[0.50,6.52] seconds)
      py311: OK (7.30=setup[0.01]+cmd[0.49,6.80] seconds)
      py312: OK (7.75=setup[0.01]+cmd[0.50,7.25] seconds)
      py313: OK (7.48=setup[0.01]+cmd[0.50,6.97] seconds)
      congratulations :) (7.79 seconds)
    ```
    
    CHANGELOG:
    - [ ] Update `noxfile.py` to ensure the environment and the python
      runtime version matches.
    - [ ] Also checking out tox for isolated and parallelized tests.
    - [ ] Fix more compatibility issues with 3.10

--- py/plugins/google-genai/test/models/test_googlegenai_gemini.py
+++ py/plugins/google-genai/test/models/test_googlegenai_gemini.py
@@ -15,7 +15,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import base64
-from enum import StrEnum
+import sys  # noqa
+
+if sys.version_info < (3, 11):  # noqa
+    from strenum import StrEnum  # noqa
+else:  # noqa
+    from enum import StrEnum  # noqa
 
 import pytest
 from google import genai
