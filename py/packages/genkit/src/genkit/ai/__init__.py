5d3081ed1dfc972df4f68a6a7a1752b51dc539a1
Yesudeep Mangalapilly <yesudeep@google.com>
2025-04-06 15:36:07 -0700

    fix(py/genkit): fix incorrect import

--- py/packages/genkit/src/genkit/ai/__init__.py
+++ py/packages/genkit/src/genkit/ai/__init__.py
@@ -21,8 +21,8 @@ including server configuration, runtime management, and client-server
 communication protocols.
 """
 
-from genkit.ai.base import GenkitRegistry
 from genkit.ai.plugin import Plugin
+from genkit.ai.registry import GenkitRegistry
 from genkit.blocks.document import Document
 from genkit.blocks.tools import ToolRunContext, tool_response
 from genkit.core import GENKIT_CLIENT_HEADER, GENKIT_VERSION
