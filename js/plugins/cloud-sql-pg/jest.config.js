dece4fdb5b6a521912d54097c3f73b7b3e6cb321
luismu <luismu19@gmail.com>
2025-05-21 16:18:23 -0500

    chore: Update Jest configuration and package dependencies
    - Simplified jest.config.js by removing unnecessary options and comments.
    - Updated test roots and match patterns for better test discovery.
    - Added @types/jest and updated typescript version in package.json.
    - Included zod as a new dependency.

--- js/plugins/cloud-sql-pg/jest.config.js
+++ js/plugins/cloud-sql-pg/jest.config.js
@@ -1,49 +1,13 @@
-/**
- * Copyright 2025 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  roots: ['<rootDir>/'],
+  testMatch: ['**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.ts$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  extensionsToTreatAsEsm: ['.ts'],
-  testEnvironmentOptions: {
-    url: 'http://localhost',
-  },
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+  ],
 };
