2f8fcaa9cf2de2bff2f9ee8724120443b078d941
Bryan Atkinson <bryanatkinson@google.com>
2025-06-13 11:01:57 -0400

    Remove action level metrics, audio/video metrics and some unused dimensions

--- js/plugins/google-cloud/src/telemetry/action.ts
+++ js/plugins/google-cloud/src/telemetry/action.ts
@@ -14,43 +14,18 @@
  * limitations under the License.
  */
 
-import { ValueType } from '@opentelemetry/api';
-import { hrTimeDuration, hrTimeToMilliseconds } from '@opentelemetry/core';
-import type { ReadableSpan } from '@opentelemetry/sdk-trace-base';
-import { GENKIT_VERSION } from 'genkit';
 import { logger } from 'genkit/logging';
 import { toDisplayPath, type PathMetadata } from 'genkit/tracing';
-import {
-  MetricCounter,
-  MetricHistogram,
-  internalMetricNamespaceWrap,
-  type Telemetry,
-} from '../metrics.js';
+import { type Telemetry } from '../metrics.js';
 import {
   createCommonLogAttributes,
-  extractErrorName,
   extractOuterFeatureNameFromPath,
   truncate,
   truncatePath,
 } from '../utils.js';
+import type { ReadableSpan } from '@opentelemetry/sdk-trace-base';
 
 class ActionTelemetry implements Telemetry {
-  /**
-   * Wraps the declared metrics in a Genkit-specific, internal namespace.
-   */
-  private _N = internalMetricNamespaceWrap.bind(null, 'action');
-
-  private actionCounter = new MetricCounter(this._N('requests'), {
-    description: 'Counts calls to genkit actions.',
-    valueType: ValueType.INT,
-  });
-
-  private actionLatencies = new MetricHistogram(this._N('latency'), {
-    description: 'Latencies when calling Genkit actions.',
-    valueType: ValueType.DOUBLE,
-    unit: 'ms',
-  });
-
   tick(
     span: ReadableSpan,
     paths: Set<PathMetadata>,
@@ -65,19 +40,6 @@ class ActionTelemetry implements Telemetry {
     let featureName = extractOuterFeatureNameFromPath(path);
     if (!featureName || featureName === '<unknown>') {
       featureName = actionName;
-    }
-    const state = attributes['genkit:state'] || 'success';
-    const latencyMs = hrTimeToMilliseconds(
-      hrTimeDuration(span.startTime, span.endTime)
-    );
-    const errorName = extractErrorName(span.events);
-
-    if (state === 'success') {
-      this.writeSuccess(actionName, featureName, path, latencyMs);
-    } else if (state === 'error') {
-      this.writeFailure(actionName, featureName, path, latencyMs, errorName);
-    } else {
-      logger.warn(`Unknown action state; ${state}`);
     }
 
     if (subtype === 'tool' && logInputAndOutput) {
@@ -111,44 +73,6 @@ class ActionTelemetry implements Telemetry {
         );
       }
     }
-  }
-
-  private writeSuccess(
-    actionName: string,
-    featureName: string,
-    path: string,
-    latencyMs: number
-  ) {
-    const dimensions = {
-      name: actionName,
-      featureName,
-      path,
-      status: 'success',
-      source: 'ts',
-      sourceVersion: GENKIT_VERSION,
-    };
-    this.actionCounter.add(1, dimensions);
-    this.actionLatencies.record(latencyMs, dimensions);
-  }
-
-  private writeFailure(
-    actionName: string,
-    featureName: string,
-    path: string,
-    latencyMs: number,
-    errorName?: string
-  ) {
-    const dimensions = {
-      name: actionName,
-      featureName,
-      path,
-      source: 'ts',
-      sourceVersion: GENKIT_VERSION,
-      status: 'failure',
-      error: errorName,
-    };
-    this.actionCounter.add(1, dimensions);
-    this.actionLatencies.record(latencyMs, dimensions);
   }
 
   private writeLog(
