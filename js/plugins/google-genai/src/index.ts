b6fed2dc9efca0e8571ee79ab8bcd0f1876119d5
Ingrid Fielker <ifielker@google.com>
2025-06-27 20:09:05 -0400

    removed comment

--- js/plugins/google-genai/src/index.ts
+++ js/plugins/google-genai/src/index.ts
@@ -14,11 +14,5 @@
  * limitations under the License.
  */
 
-/**
- * @fileoverview A description of this module.  What would someone
- * new to your team want to know about the code in this file?
- * (DO NOT SUBMIT as is; replace this comment.)
- */
-
 //export { googleAI } from './googleai';
 export { vertexAI } from './vertexai';
