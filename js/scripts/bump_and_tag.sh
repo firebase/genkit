fa8f9aed7eb76a56b16ca1f2a2a2b385de67accd
pavelgj <pavelgj@gmail.com>
2025-07-10 10:50:18 -0400

    chore: new js bump version and tag script

---
+++ js/scripts/bump_and_tag.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this script lets you bump and tag a specifific package
+# js/scripts/bump_and_tag.sh genkit-tools/cli genkit-cli prerelease rc
+
+set -e
+set -x
+
+PACKAGE_PATH=$1
+PACKAGE_NAME=$2
+RELEASE_TYPE=$3
+PREID="${4:-rc}"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# import bump_version script
+. $SCRIPT_DIR/bump_version.sh
+
+COMMIT_MSG="chore: CLI version bump$NEWLINE$NEWLINE"
+
+bump_version $PACKAGE_PATH $PACKAGE_NAME
+
+commit_and_tag
