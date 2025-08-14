42b7ae31796714f570797e7e804987ff9347dffa
Marcel Folaron <marcel@leantime.io>
2025-08-14 11:18:36 -0400

    update testapp name parameter to stepName

--- js/testapps/basic-gemini/src/index.ts
+++ js/testapps/basic-gemini/src/index.ts
@@ -40,7 +40,7 @@ export const jokeFlow = ai.defineFlow(
   },
   async () => {
     const llmResponse = await ai.generate({
-      name: 'joke-creator',
+      stepName: 'joke-creator',
       model: gemini15Flash,
       config: {
         temperature: 2,
