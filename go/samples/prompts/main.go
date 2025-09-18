344afedfe29e8e5dfb5b319cd40c9a450419e000
Alex Pascal <apascal07@gmail.com>
2025-09-18 13:50:44 -0700

    Update main.go

--- go/samples/prompts/main.go
+++ go/samples/prompts/main.go
@@ -34,7 +34,7 @@ import (
 func main() {
 	ctx := context.Background()
 	g := genkit.Init(ctx,
-		genkit.WithDefaultModel("googleai/gemini-2.0-flash"),
+		genkit.WithDefaultModel("googleai/gemini-2.5-flash"),
 		genkit.WithPlugins(&googlegenai.GoogleAI{}),
 		genkit.WithPromptDir("prompts"),
 	)
