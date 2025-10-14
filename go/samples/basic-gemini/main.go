e7f8b0c45b33fa4bdeb2ec771efb362039e952d7
Alex Pascal <apascal07@gmail.com>
2025-10-14 12:42:16 -0700

    Update main.go

--- go/samples/basic-gemini/main.go
+++ go/samples/basic-gemini/main.go
@@ -1,60 +1,55 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
 	"github.com/firebase/genkit/go/plugins/googlegenai"
+	"google.golang.org/genai"
 )
 
 func main() {
 	ctx := context.Background()
-	g := genkit.Init(ctx,
-		genkit.WithPlugins(&googlegenai.GoogleAI{}),
-		genkit.WithDefaultModel("googleai/gemini-2.5-flash"),
-	)
 
-	inputSchema := map[string]any{
-		"type": "object",
-		"properties": map[string]any{
-			"id": map[string]any{
-				"name":        "id",
-				"description": "The id to be printed.",
-				"type":        "integer",
-			}},
-		"required": []any{"id"},
-	}
+	// Initialize Genkit with the Google AI plugin. When you pass nil for the
+	// Config parameter, the Google AI plugin will get the API key from the
+	// GEMINI_API_KEY or GOOGLE_API_KEY environment variable, which is the recommended
+	// practice.
+	g := genkit.Init(ctx, genkit.WithPlugins(&googlegenai.GoogleAI{}))
 
-	executeFn := func(ctx *ai.ToolContext, input any) (string, error) {
-		inputMap, ok := input.(map[string]any)
-		if !ok {
-			// If the input is not a map, return an error indicating the type mismatch.
-			log.Fatalf("tool input expected map[string]any, got %T", input)
+	// Define a simple flow that generates jokes about a given topic
+	genkit.DefineStreamingFlow(g, "jokesFlow", func(ctx context.Context, input string, cb ai.ModelStreamCallback) (string, error) {
+		resp, err := genkit.Generate(ctx, g,
+			ai.WithModelName("googleai/gemini-2.5-flash"),
+			ai.WithConfig(&genai.GenerateContentConfig{
+				Temperature: genai.Ptr[float32](1.0),
+				ThinkingConfig: &genai.ThinkingConfig{
+					ThinkingBudget: genai.Ptr[int32](0),
+				},
+			}),
+			ai.WithStreaming(cb),
+			ai.WithPrompt(`Tell short jokes about %s`, input))
+		if err != nil {
+			return "", err
 		}
-		id := inputMap["id"]
-		fmt.Printf("input received of type '%T'", id)
 
-		strResult := fmt.Sprintf("%v", id)
-		return strResult, nil
-	}
-
-	genkitTool := genkit.DefineToolWithInputSchema(
-		g,
-		"example_tool",
-		"prints the id given to the tool",
-		inputSchema,
-		executeFn,
-	)
-
-	resp, err := genkitTool.RunRaw(ctx, map[string]any{
-		"id": int64(1),
+		return resp.Text(), nil
 	})
-	if err != nil {
-		log.Fatal("error invoking the tool")
-	}
-	fmt.Println(resp)
 
+	<-ctx.Done()
 }
