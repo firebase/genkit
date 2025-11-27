bb9bc233cae6d050d206121ada6b0156e80f9f93
Hugo Aguirre Parra <aguirreparra@google.com>
2025-11-27 00:01:16 +0000

    update sample

--- go/samples/compat_oai/deepseek/main.go
+++ go/samples/compat_oai/deepseek/main.go
@@ -1,23 +1,15 @@
 // Copyright 2024 Google LLC
 // SPDX-License-Identifier: Apache-2.0
 
-// This program can be manually tested like so:
-// Start the server listening on port 3100:
-//
-//	genkit start -o -- go run .
-
 package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net/http"
+	"math"
 
 	"github.com/firebase/genkit/go/ai"
 	"github.com/firebase/genkit/go/genkit"
 	oai_deepseek "github.com/firebase/genkit/go/plugins/compat_oai/deepseek"
-	"github.com/firebase/genkit/go/plugins/server"
 	"github.com/openai/openai-go"
 )
 
@@ -26,19 +18,29 @@ func main() {
 
 	// DEEPSEEK_API_KEY and DEEPSEEK_BASE_URL environment values will be read automatically unless
 	// they are provided via `option.RequestOption{}`
-	ds := oai_deepseek.DeepSeek{}
+	g := genkit.Init(ctx,
+		genkit.WithPlugins(&oai_deepseek.DeepSeek{}),
+		genkit.WithDefaultModel("deepseek/deepseek-chat"))
 
-	g := genkit.Init(ctx, genkit.WithPlugins(&ds))
+	// Define a tool for calculating gablorkens
+	gablorkenTool := genkit.DefineTool(g, "gablorken", "use when need to calculate a gablorken",
+		func(ctx *ai.ToolContext, input struct {
+			Value float64
+			Over  float64
+		},
+		) (float64, error) {
+			return math.Pow(input.Value, input.Over), nil
+		},
+	)
 
-	genkit.DefineFlow(g, "basic", func(ctx context.Context, subject string) (string, error) {
-		dsChat := ds.Model(g, "deepseek-chat")
-
-		prompt := fmt.Sprintf("tell me a short story joke about %s", subject)
+	genkit.DefineStreamingFlow(g, "streaming tool", func(ctx context.Context, input any, cb ai.ModelStreamCallback) (string, error) {
 		config := &openai.ChatCompletionNewParams{Temperature: openai.Float(0.5)}
 		resp, err := genkit.Generate(ctx,
 			g,
-			ai.WithModel(dsChat),
-			ai.WithPrompt(prompt),
+			//			ai.WithModel(dsChat),
+			ai.WithPrompt("calculate the gablorken of value 3 over 5"),
+			ai.WithTools(gablorkenTool),
+			ai.WithStreaming(cb),
 			ai.WithConfig(config))
 		if err != nil {
 			return "", err
@@ -46,9 +48,5 @@ func main() {
 		return resp.Text(), nil
 	})
 
-	mux := http.NewServeMux()
-	for _, a := range genkit.ListFlows(g) {
-		mux.HandleFunc("POST /"+a.Name(), genkit.Handler(a))
-	}
-	log.Fatal(server.Start(ctx, "127.0.0.1:8080", mux))
+	<-ctx.Done()
 }
