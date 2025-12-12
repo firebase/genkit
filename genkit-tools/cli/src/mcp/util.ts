2c01e4149c35786b5a0370dc95ce963e22c89483
Jeff Huang <huangjeff@google.com>
2025-12-11 20:38:51 -0600

    fix

--- genkit-tools/cli/src/mcp/util.ts
+++ genkit-tools/cli/src/mcp/util.ts
@@ -36,7 +36,7 @@ export class McpRuntimeManager {
 
   async getManagerWithDevProcess(command: string, args: string[]) {
     if (this.manager) {
-      await this.manager.processManager?.kill();
+      await this.manager.stop();
     }
     const devManager = await startDevProcessManager(
       this.projectRoot,
@@ -49,7 +49,7 @@ export class McpRuntimeManager {
 
   async kill() {
     if (this.manager) {
-      await this.manager.processManager?.kill();
+      await this.manager.stop();
     }
   }
 }
