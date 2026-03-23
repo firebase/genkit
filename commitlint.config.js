8e48522c3b11e26c0a47473bf6fba24b647cbd45
Pavel Jbanov <pavelgj@gmail.com>
2026-03-23 14:54:01 -0400

    fmt

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,17 +21,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat',     // New feature
-        'fix',      // Bug fix
-        'docs',     // Documentation only changes
-        'style',    // Code style changes (formatting, etc.)
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation only changes
+        'style', // Code style changes (formatting, etc.)
         'refactor', // Code refactoring
-        'perf',     // Performance improvements
-        'test',     // Adding or updating tests
-        'build',    // Build system or external dependencies
-        'ci',       // CI configuration changes
-        'chore',    // Other changes that don't modify src or test files
-        'revert',   // Revert a previous commit
+        'perf', // Performance improvements
+        'test', // Adding or updating tests
+        'build', // Build system or external dependencies
+        'ci', // CI configuration changes
+        'chore', // Other changes that don't modify src or test files
+        'revert', // Revert a previous commit
       ],
     ],
     'subject-case': [0], // Level 0 ignores the rule
